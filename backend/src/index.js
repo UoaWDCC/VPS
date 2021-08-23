51963ad1f6f379aff763b6e65c65dbf61857abfe
Lucas Gao <lucas2005gao@gmail.com>
2021-08-23 15:03:25 +1200

    🚀 NEW RELEASE: prettier and eslint all passing

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import { join } from "path";
 import connectToDatabase from "./db/db-connect";
 
+// Setup our routes.
+import routes from "./routes";
+
 // Setup Express
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,9 +13,6 @@ const port = process.env.PORT || 3000;
 // Setup body-parser
 app.use(express.json());
 app.use(cors());
-
-// Setup our routes.
-import routes from "./routes";
 app.use("/", routes);
 
 // Make the "public" folder available statically
@@ -20,18 +20,15 @@ app.use(express.static(join(__dirname, "../public")));
 
 // Serve up the frontend's "build" directory, if we're running in production mode.
 if (process.env.NODE_ENV === "production") {
+  // eslint-disable-next-line no-console
   console.log("Running in production!");
 
   // Make all files in that folder public
   app.use(express.static(express.join(__dirname, "../../frontend/build")));
-
-  // If we get any GET request we can't process using one of the server routes, serve up index.html by default.
-  app.get("*", function (req, res) {
-    res.sendFile(express.join(__dirname, "../../frontend/build/index.html"));
-  });
 }
 
 // Start the DB running. Then, once it's connected, start the server.
 connectToDatabase().then(function () {
+  // eslint-disable-next-line no-console
   app.listen(port, () => console.log(`App server listening on port ${port}!`));
 });
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 
-const router = Router();
-
 import api from "./api";
+
+const router = Router();
 router.use("/api", api);
 
 export default router;
