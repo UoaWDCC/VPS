7b2f1befb366ec01c41afa162aa53dc996f27eab
Andrew <afei088@aucklanduni.ac.nz>
2021-08-23 19:38:58 +1200

    [VPS-5] added logo to side bar

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -3,7 +3,13 @@ import React from "react";
 export default function SideBar() {
   return (
     <>
-      <div className="side-bar" />
+      <div className="side-bar">
+        <img
+          className="logo"
+          src="uoa-med-and-health-sci-logo.png"
+          alt="UoA Medical & Health Science Logo"
+        />
+      </div>
     </>
   );
 }
