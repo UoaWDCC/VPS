ae2f3abf844dddabdc4f4d91cfaab1dc3e999318
dongmeilim <dongmeilim@gmail.com>
2021-08-25 18:10:56 +1200

    [VPS-6] add and update snapshot tests

--- frontend/src/context/ScenarioContextProvider.js
+++ frontend/src/context/ScenarioContextProvider.js
@@ -1,31 +1,48 @@
 import React from "react";
 import ScenarioContext from "./ScenarioContext";
 
-export default function ScenarioContextProvider({ children }) {
-  const dummyData = [
-    {
-      id: 1,
-      name: "Scenario 1",
-      img: "https://unidirectory.auckland.ac.nz/people/imageraw/n-giacaman/11018090/biggest",
-    },
-    {
-      id: 2,
-      name: "Scenario 2",
-      img: "https://unidirectory.auckland.ac.nz/people/imageraw/reza-shahamiri/11450631/biggest",
-    },
-  ];
+export default function ScenarioContextProvider({ useTestData, children }) {
+  let data;
+  if (useTestData) {
+    data = [
+      {
+        id: 1,
+        name: "Scenario 1",
+        img: "https://unidirectory.auckland.ac.nz/people/imageraw/n-giacaman/11018090/biggest",
+      },
+      {
+        id: 2,
+        name: "Scenario 2",
+        img: "https://unidirectory.auckland.ac.nz/people/imageraw/reza-shahamiri/11450631/biggest",
+      },
+    ];
 
-  for (let i = 3; i < 30; i += 1) {
-    dummyData.push({
-      id: i,
-      name: `Scenario ${i}`,
-      img: "",
-    });
+    for (let i = 3; i < 30; i += 1) {
+      data.push({
+        id: i,
+        name: `Scenario ${i}`,
+        img: "",
+      });
+    }
+  } else {
+    // TODO replace with database call
+    data = [
+      {
+        id: 1,
+        name: "Scenario 1",
+        img: "https://unidirectory.auckland.ac.nz/people/imageraw/n-giacaman/11018090/biggest",
+      },
+      {
+        id: 2,
+        name: "Scenario 2",
+        img: "https://unidirectory.auckland.ac.nz/people/imageraw/reza-shahamiri/11450631/biggest",
+      },
+    ];
   }
   return (
     <ScenarioContext.Provider
       value={{
-        scenarios: dummyData,
+        scenarios: data,
       }}
     >
       {children}
