c81079f5373b6c502cf80d6cd2680f32c62c6242
dongmeilim <dongmeilim@gmail.com>
2021-08-26 00:53:11 +1200

    [VPS-19] Update context on selecting an item

--- frontend/src/components/ListContainer.js
+++ frontend/src/components/ListContainer.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ImageList from "@material-ui/core/ImageList";
 import ImageListItem from "@material-ui/core/ImageListItem";
 import { Box } from "@material-ui/core";
 import styles from "../styling/ListContainer.module.scss";
 
-export default function ListContainer({ data }) {
+export default function ListContainer({ data, onItemSelected }) {
+  const [selected, setSelected] = useState();
+  const onItemClick = (item) => {
+    setSelected(item.id);
+    onItemSelected(item);
+  };
   return (
     <>
       <div className={styles.scenarioListContainer}>
         {data.length > 0 ? (
           <ImageList rowHeight={210} cols={4} gap={30}>
             {data.map((item) => (
-              <ImageListItem key={item.id} cols={1} height={200}>
+              <ImageListItem
+                key={item.id}
+                cols={1}
+                height={200}
+                onClick={() => onItemClick(item)}
+              >
                 <div className={styles.imageListItem}>
                   <Box
                     height={160}
-                    border={1}
+                    border={5}
                     borderRadius={10}
-                    borderColor="#747474"
+                    borderColor={item.id === selected ? "#008a7b" : "#747474"}
                     overflow="hidden"
                     bgcolor="#f1f1f1"
                     textAlign="center"
