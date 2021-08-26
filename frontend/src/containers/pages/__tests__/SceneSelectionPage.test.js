6a779e024f873944ccd179d47c9d123fcc5c379b
dongmeilim <dongmeilim@gmail.com>
2021-08-26 12:18:01 +1200

    [VPS-19] Update snapshot tests

--- frontend/src/containers/pages/__tests__/SceneSelectionPage.test.js
+++ frontend/src/containers/pages/__tests__/SceneSelectionPage.test.js
@@ -2,11 +2,17 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { BrowserRouter } from "react-router-dom";
 import SceneSelectionPage from "../SceneSelectionPage";
+import ScenarioContextProvider from "../../../context/ScenarioContextProvider";
+import SceneContextProvider from "../../../context/SceneContextProvider";
 
 test("Scene Selection page snapshot test", () => {
   const component = renderer.create(
     <BrowserRouter>
-      <SceneSelectionPage />
+      <ScenarioContextProvider>
+        <SceneContextProvider>
+          <SceneSelectionPage useTestData />
+        </SceneContextProvider>
+      </ScenarioContextProvider>
     </BrowserRouter>
   );
 
