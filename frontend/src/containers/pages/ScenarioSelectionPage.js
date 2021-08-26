01e929f65020b6949ac73b868cfdbadfbddd9c7c
dongmeilim <dongmeilim@gmail.com>
2021-08-26 12:04:36 +1200

    [VPS-19] Update pages with contexts and components

--- frontend/src/containers/pages/ScenarioSelectionPage.js
+++ frontend/src/containers/pages/ScenarioSelectionPage.js
@@ -1,18 +1,48 @@
 import React, { useContext, useEffect } from "react";
 import SideBar from "../../components/SideBar";
 import ListContainer from "../../components/ListContainer";
-import RowContainer from "../../components/RowContainer";
+import ScreenContainer from "../../components/ScreenContainer";
 import ScenarioContext from "../../context/ScenarioContext";
 
-export default function ScenarioSelectionPage() {
-  const { scenarios, setScenario } = useContext(ScenarioContext);
+const axios = require("axios");
+
+export default function ScenarioSelectionPage({ useTestData }) {
+  const { scenarios, setScenarios, setCurrentScenario } =
+    useContext(ScenarioContext);
   useEffect(() => {
-    setScenario(null);
+    setCurrentScenario(null);
   }, []);
+  useEffect(() => {
+    if (useTestData) {
+      // fill with dummy data
+      const testData = [];
+      for (let i = 1; i < 30; i += 1) {
+        testData.push({
+          id: i,
+          name: `Scenario ${i}`,
+        });
+      }
+      setScenarios(testData);
+    } else {
+      // fetch scenarios
+      axios.get("api/scenario").then((response) => {
+        const processedData = [];
+        response.data.map((item) =>
+          processedData.push({
+            // eslint-disable-next-line dot-notation
+            id: item["_id"],
+            name: item.name,
+          })
+        );
+        setScenarios(processedData);
+      });
+    }
+  }, []);
+
   return (
-    <RowContainer>
+    <ScreenContainer>
       <SideBar />
-      <ListContainer data={scenarios} onItemSelected={setScenario} />
-    </RowContainer>
+      <ListContainer data={scenarios} onItemSelected={setCurrentScenario} />
+    </ScreenContainer>
   );
 }
--- frontend/src/containers/pages/SceneSelectionPage.js
+++ frontend/src/containers/pages/SceneSelectionPage.js
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import TopBar from "../../components/TopBar";
+import ListContainer from "../../components/ListContainer";
+import ScreenContainer from "../../components/ScreenContainer";
+import SceneContext from "../../context/SceneContext";
+import ScenarioContext from "../../context/ScenarioContext";
 
-export default function SceneSelectionPage() {
+const axios = require("axios");
+
+export default function SceneSelectionPage({ useTestData }) {
+  const { scenes, setScenes, setCurrentScene } = useContext(SceneContext);
+  const { currentScenario } = useContext(ScenarioContext);
+  useEffect(() => {
+    if (useTestData) {
+      // fill with dummy data
+      const testData = [];
+      for (let i = 1; i < 30; i += 1) {
+        testData.push({
+          id: i,
+          name: `Scene ${i}`,
+        });
+      }
+      setScenes(testData);
+    } else if (currentScenario) {
+      // fetch scenarios
+      axios
+        .get(`/api/scenario/${currentScenario.id}/scene`)
+        .then((response) => {
+          const processedData = [];
+          response.data.map((item) =>
+            processedData.push({
+              // eslint-disable-next-line dot-notation
+              id: item["_id"],
+              name: item.name,
+            })
+          );
+          setScenes(processedData);
+        });
+    } else {
+      // TODO remove this else clause when Create button is implemented
+      setScenes([]);
+    }
+  }, []);
   return (
-    <>
+    <ScreenContainer vertical>
       <TopBar />
-    </>
+      <ListContainer
+        data={scenes}
+        onItemSelected={setCurrentScene}
+        placeholderText="No Scenes"
+        wide
+      />
+    </ScreenContainer>
   );
 }
