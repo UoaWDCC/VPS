2e3249220d65de02a2efb83d774a9dafe022ad1e
Lucas Gao <lucas2005gao@gmail.com>
2021-09-11 01:11:03 +1200

    [VPS-67] Updated ToolBar to have the correct submenu

--- frontend/src/containers/pages/AuthoringTool/ToolBar/ToolBarData.js
+++ frontend/src/containers/pages/AuthoringTool/ToolBar/ToolBarData.js
@@ -2,8 +2,6 @@ import React from "react";
 import ImageIcon from "@material-ui/icons/Image";
 import TextFieldsIcon from "@material-ui/icons/TextFields";
 import VolumeUpIcon from "@material-ui/icons/VolumeUp";
-import CloudQueueIcon from "@material-ui/icons/CloudQueue";
-import PublishIcon from "@material-ui/icons/Publish";
 import ChooseBackgroundSubMenu from "./Background/ChooseBackgroundSubMenu";
 
 const toolBarData = [
@@ -20,28 +18,15 @@ const toolBarData = [
     title: "Text",
     icon: <TextFieldsIcon fontSize="medium" />,
     onClick: () => {
-      console.log("audio clicked");
+      console.log("text clicked");
     },
   },
   {
     title: "Audio",
     icon: <VolumeUpIcon fontSize="medium" />,
-    dropdown: [
-      {
-        title: "choose from bank",
-        icon: <CloudQueueIcon fontSize="medium" />,
-        onClick: () => {
-          console.log("choose from bank");
-        },
-      },
-      {
-        title: "upload",
-        icon: <PublishIcon fontSize="medium" />,
-        onClick: () => {
-          console.log("upload");
-        },
-      },
-    ],
+    onClick: () => {
+      console.log("audio clicked");
+    },
   },
 ];
 
