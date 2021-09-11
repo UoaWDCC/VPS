2d63d2a5eebddeddd41e737b4e481c8cb31c6894
Annie Chau <annithinggoes@gmail.com>
2021-09-12 03:35:39 +1200

    [VPS-030] Styling applied

--- frontend/src/components/ShareModal.js
+++ frontend/src/components/ShareModal.js
@@ -1,71 +1,33 @@
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
-import MuiDialogTitle from "@material-ui/core/DialogTitle";
-import IconButton from "@material-ui/core/IconButton";
-import CloseIcon from "@material-ui/icons/Close";
 import Typography from "@material-ui/core/Typography";
 import React, { useContext, useState } from "react";
-import {
-  DialogActions,
-  DialogContent,
-  TextField,
-  withStyles,
-} from "@material-ui/core";
+import { DialogContent, DialogTitle, TextField } from "@material-ui/core";
 import ScenarioContext from "../context/ScenarioContext";
-// import styles from "../styling/ScreenContainer.module.scss";
+import styles from "../styling/ShareModal.module.scss";
 
 export default function ShareModal({ isOpen, handleClose }) {
   const { currentScenario } = useContext(ScenarioContext);
   const [copySuccess, setCopySuccess] = useState(false);
   const url = `${window.location.origin}/play/${currentScenario._id}`;
 
-  const styles = (theme) => ({
-    root: {
-      margin: 0,
-      padding: theme.spacing(2),
-    },
-    closeButton: {
-      position: "absolute",
-      right: theme.spacing(1),
-      top: theme.spacing(1),
-      color: theme.palette.grey[500],
-    },
-  });
-
-  const DialogTitle = withStyles(styles)((props) => {
-    const { children, classes, onClose, ...other } = props;
-    return (
-      <MuiDialogTitle disableTypography className={classes.root} {...other}>
-        <Typography variant="h6">{children}</Typography>
-        {onClose ? (
-          <IconButton
-            aria-label="close"
-            className={classes.closeButton}
-            onClick={onClose}
-          >
-            <CloseIcon />
-          </IconButton>
-        ) : null}
-      </MuiDialogTitle>
-    );
-  });
-
   return (
     <div>
       <Dialog
+        className={styles.dialog}
         onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
         open={isOpen}
         fullWidth="sm"
       >
-        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+        <DialogTitle className={styles.dialogTitle} onClose={handleClose}>
           Shareable Link
         </DialogTitle>
-        <DialogContent>
-          <Typography>
+        <DialogContent className={styles.dialogContent}>
+          <Typography className={styles.dialogItem}>
             Give this link to others so they can play this scenario
           </Typography>
           <TextField
+            className={`${styles.textField} ${styles.dialogItem}`}
             defaultValue={url}
             InputProps={{
               readOnly: true,
@@ -75,6 +37,7 @@ export default function ShareModal({ isOpen, handleClose }) {
             helperText={copySuccess ? "Copied!" : ""}
           />
           <Button
+            className={`btn contained white ${styles.dialogItem}`}
             autoFocus
             onClick={() => {
               navigator.clipboard.writeText(url);
