4967d79739d0c97cc72c832e00db9e1fa624892e
Annie Chau <annithinggoes@gmail.com>
2021-09-12 04:28:06 +1200

    [CPS-066] Create back confirmation modal + styling

---
+++ frontend/src/components/BackModal.js
@@ -0,0 +1,52 @@
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import Typography from "@material-ui/core/Typography";
+import React, { useContext } from "react";
+import { DialogActions, DialogContent, DialogTitle } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import styles from "../styling/BackModal.module.scss";
+import ScenarioContext from "../context/ScenarioContext";
+
+export default function BackModal({ isOpen, handleClose }) {
+  const { currentScenario } = useContext(ScenarioContext);
+  return (
+    <div>
+      <Dialog
+        className={styles.dialog}
+        onClose={handleClose}
+        open={isOpen}
+        maxWidth="xs"
+      >
+        <DialogTitle className={styles.dialogTitle} onClose={handleClose}>
+          You have unsaved changes
+        </DialogTitle>
+        <DialogContent className={styles.dialogBody}>
+          <Typography>
+            Are you sure you want to leave? Unsaved changes will be lost.
+          </Typography>
+        </DialogContent>
+        <DialogActions
+          className={`${styles.dialogBody} ${styles.dialogActions} `}
+        >
+          <Button
+            className="btn contained red"
+            autoFocus
+            component={Link}
+            to={`/scenario/${currentScenario._id}`}
+            color="primary"
+          >
+            Yes, discard changes
+          </Button>
+          <Button
+            className="btn contained white"
+            autoFocus
+            color="primary"
+            onClick={handleClose}
+          >
+            No, keep editing
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
