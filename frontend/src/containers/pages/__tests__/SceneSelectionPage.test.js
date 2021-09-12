3bbd302725f65629d7f76057902310ec339ba45c
Annie Chau <annithinggoes@gmail.com>
2021-09-12 15:32:37 +1200

    [VPS-066] lint

--- frontend/src/containers/pages/__tests__/SceneSelectionPage.test.js
+++ frontend/src/containers/pages/__tests__/SceneSelectionPage.test.js
@@ -3,7 +3,6 @@ import renderer from "react-test-renderer";
 import { BrowserRouter } from "react-router-dom";
 import { SceneSelectionPage } from "../SceneSelectionPage";
 import SceneContextProvider from "../../../context/SceneContextProvider";
-import ScenarioContextProvider from "../../../context/ScenarioContextProvider";
 import ScenarioContext from "../../../context/ScenarioContext";
 
 const dummyScenes = [
