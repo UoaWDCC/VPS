12f5240843318d1f61841599f78f9edbf2725aea
Annie Chau <annithinggoes@gmail.com>
2021-09-12 14:29:18 +1200

    [VPS-066] Update test
    Co-authored-by: David Xiao <dxia063@aucklanduni.ac.nz>

--- frontend/src/containers/pages/AuthoringTool/__tests__/BackModal.test.js
+++ frontend/src/containers/pages/AuthoringTool/__tests__/BackModal.test.js
@@ -1,22 +1,24 @@
 import React from "react";
-import renderer from "react-test-renderer";
 import { BrowserRouter } from "react-router-dom";
+import { render } from "@testing-library/react";
 import BackModal from "../BackModal";
 import ScenarioContext from "../../../../context/ScenarioContext";
+import SceneContextProvider from "../../../../context/SceneContextProvider";
 
 test("BackModal component snapshot test", () => {
   const context = {
-    currentScenario: { _id: "test" },
+    currentScenario: { _id: "scenarioId" },
   };
 
-  const component = renderer.create(
+  const { baseElement } = render(
     <BrowserRouter>
       <ScenarioContext.Provider value={context}>
-        <BackModal isOpen />
+        <SceneContextProvider>
+          <BackModal isOpen />
+        </SceneContextProvider>
       </ScenarioContext.Provider>
     </BrowserRouter>
   );
 
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  expect(baseElement).toMatchSnapshot();
 });
