acb4197f5b9a73feb8a3888aa99d2e0f06c054e1
Andrew <afei088@aucklanduni.ac.nz>
2021-09-15 11:01:27 +1200

    [VPS-35] added button icon to tool bar and added button component to scene context on button icon click

---
+++ frontend/src/containers/pages/AuthoringTool/ToolBar/Button/addButton.js
@@ -0,0 +1,20 @@
+function addButton(currentScene, setCurrentScene) {
+  const newButton = {
+    type: "BUTTON",
+    text: "Button",
+    border: true,
+    left: "50%",
+    top: "50%",
+  };
+
+  const updatedComponents = currentScene.components;
+
+  updatedComponents.push(newButton);
+
+  setCurrentScene({
+    ...currentScene,
+    components: updatedComponents,
+  });
+}
+
+export default addButton;
--- frontend/src/containers/pages/AuthoringTool/ToolBar/__tests__/ToolBar.test.js
+++ frontend/src/containers/pages/AuthoringTool/ToolBar/__tests__/ToolBar.test.js
@@ -1,12 +1,18 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import { BrowserRouter } from "react-router-dom";
+import ScenarioContextProvider from "../../../../../context/ScenarioContextProvider";
+import SceneContextProvider from "../../../../../context/SceneContextProvider";
 import ToolBar from "../ToolBar";
 
 test("ToolBar component snapshot test", () => {
   const component = renderer.create(
     <BrowserRouter>
-      <ToolBar />
+      <ScenarioContextProvider>
+        <SceneContextProvider>
+          <ToolBar />
+        </SceneContextProvider>
+      </ScenarioContextProvider>
     </BrowserRouter>
   );
 
