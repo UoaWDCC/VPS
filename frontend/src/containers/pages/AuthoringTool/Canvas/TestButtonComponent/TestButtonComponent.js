6172ad66f1363bd186857acf90ad8eaeee7e9047
David Xiao <davidjinxiao@hotmail.com>
2021-09-14 15:11:29 +1200

    [VPS-71] made test button use make styles, and removed duplicate test button component

--- frontend/src/containers/pages/AuthoringTool/Canvas/TestButtonComponent/TestButtonComponent.js
+++ frontend/src/containers/pages/AuthoringTool/Canvas/TestButtonComponent/TestButtonComponent.js
@@ -1,11 +1,17 @@
-import { Button } from "@material-ui/core";
+import { Button, makeStyles } from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles({
+  testButton: {
+    position: "absolute",
+  },
+});
+
 export default function TestButtonComponent({ id, selectElement }) {
+  const styles = useStyles();
   return (
     <Button
-      className="btn top contained white"
-      style={{ position: "absolute" }}
+      className={`btn top contained white ${styles.testButton}`}
       color="default"
       variant="contained"
       id={id}
