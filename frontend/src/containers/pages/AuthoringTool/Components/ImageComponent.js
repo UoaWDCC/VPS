52955caacb0339e7992d7e5d7cb27e6a86f01ae7
David Xiao <davidjinxiao@hotmail.com>
2021-09-20 19:08:15 +1200

    [VPS-98] fixed intial image sizeing problem by calculating height on init

--- frontend/src/containers/pages/AuthoringTool/Components/ImageComponent.js
+++ frontend/src/containers/pages/AuthoringTool/Components/ImageComponent.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+/* eslint-disable no-param-reassign */
+import React, { useEffect, useState } from "react";
 import Image from "material-ui-image";
 import { useGet } from "../../../../hooks/crudHooks";
 import { imageStyles, imageContainerStyles } from "./components.styles";
@@ -6,6 +7,16 @@ import { imageStyles, imageContainerStyles } from "./components.styles";
 export default function ImageComponent({ id, onClick, component }) {
   const [image, setImage] = useState();
   useGet(`/api/image/${component.imageId}`, setImage);
+
+  useEffect(() => {
+    // The below is used to recalculate the height and width of an image. Especially when it is intiallised using auto
+    const imageElement = document.getElementById(id);
+    const canvas = document.getElementById("canvas")?.getBoundingClientRect();
+    if (canvas && component && imageElement) {
+      component.width = (imageElement.offsetWidth / canvas.width) * 100;
+      component.height = (imageElement.offsetHeight / canvas.height) * 100;
+    }
+  }, [image]);
 
   return (
     <Image
