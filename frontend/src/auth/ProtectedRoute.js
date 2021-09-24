ba166bf3a16de592ab3cc90086721c4087908be5
Lucas Gao <lucas2005gao@gmail.com>
2021-09-24 22:12:04 +1200

    [VPS-89] ProtectedRoute updated to work after merge

--- frontend/src/auth/ProtectedRoute.js
+++ frontend/src/auth/ProtectedRoute.js
@@ -3,18 +3,18 @@ import { Route, Redirect } from "react-router-dom";
 import AuthenticationContext from "../context/AuthenticationContext";
 import LoadingPage from "../containers/pages/LoadingPage";
 
-function ProtectedRoute({ component: Component, ...rest }) {
+function ProtectedRoute({ children, ...rest }) {
   const { loading, user } = useContext(AuthenticationContext);
 
   return (
     <Route
       {...rest}
-      render={(props) => {
+      render={() => {
         if (loading) {
           return <LoadingPage text="Loading contents..." />;
         }
         if (user) {
-          return <Component {...props} {...rest} />;
+          return children;
         }
         return (
           <Redirect
