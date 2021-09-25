cf55e3d283ba71ac135732ce0b3c88fd286ae3e5
Annie Chau <annithinggoes@gmail.com>
2021-09-26 01:00:36 +1200

    [VPS-100] Add auth to scene routes

--- backend/src/routes/api/scene.js
+++ backend/src/routes/api/scene.js
@@ -8,6 +8,8 @@ import {
   deleteScene,
   duplicateScene,
 } from "../../db/daos/sceneDao";
+import auth from "../../middleware/firebase-auth";
+import scenarioAuth from "../../middleware/scenario-auth";
 
 const router = Router({ mergeParams: true });
 
@@ -15,12 +17,11 @@ const HTTP_OK = 200;
 const HTTP_NO_CONTENT = 204;
 const HTTP_NOT_FOUND = 404;
 
-router.post("/", async (req, res) => {
-  const { name, components } = req.body;
+// Get scene infromation
+router.get("/full/:sceneId", async (req, res) => {
+  const scene = await retrieveScene(req.params.sceneId);
 
-  const scene = await createScene(req.params.scenarioId, { name, components });
-
-  res.status(HTTP_OK).json(scene);
+  res.json(scene);
 });
 
 router.get("/", async (req, res) => {
@@ -29,13 +30,22 @@ router.get("/", async (req, res) => {
   res.json(scenes);
 });
 
-router.get("/full/:sceneId", async (req, res) => {
-  const scene = await retrieveScene(req.params.sceneId);
+// Apply auth middleware to all routes below this point
+router.use(auth);
+// // Apply scenario auth middleware
+router.use(scenarioAuth);
 
-  res.json(scene);
+router.post("/", async (req, res) => {
+  console.log(`post scene ${req.body.uid}`);
+  const { name, components } = req.body;
+
+  const scene = await createScene(req.params.scenarioId, { name, components });
+
+  res.status(HTTP_OK).json(scene);
 });
 
 router.put("/:sceneId", async (req, res) => {
+  console.log(`put scene ${req.body.uid}`);
   const { name, components } = req.body;
 
   const scene = await updateScene(req.params.sceneId, { name, components });
@@ -44,6 +54,7 @@ router.put("/:sceneId", async (req, res) => {
 });
 
 router.delete("/:sceneId", async (req, res) => {
+  console.log(`delete scene ${req.body.uid}`);
   const deleted = await deleteScene(req.params.scenarioId, req.params.sceneId);
 
   if (deleted) {
@@ -54,6 +65,7 @@ router.delete("/:sceneId", async (req, res) => {
 });
 
 router.post("/duplicate/:sceneId", async (req, res) => {
+  console.log(`dup scene ${req.body.uid}`);
   const scene = await duplicateScene(req.params.scenarioId, req.params.sceneId);
 
   res.status(HTTP_OK).json(scene);
