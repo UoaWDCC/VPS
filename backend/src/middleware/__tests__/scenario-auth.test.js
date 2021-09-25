7199ab4f96deb3a07c78eaa9eaf2edb596167f2e
Annie Chau <annithinggoes@gmail.com>
2021-09-26 03:12:46 +1300

    [VPS-100] scenario-auth test

---
+++ backend/src/middleware/__tests__/scenario-auth.test.js
@@ -0,0 +1,80 @@
+/* eslint-disable no-underscore-dangle */
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import Scenario from "../../db/models/scenario";
+import scenarioAuth from "../scenario-auth";
+
+describe("Scenario Auth Middleware tests", () => {
+  const HTTP_UNAUTHORISED = 401;
+  const HTTP_NOT_FOUND = 404;
+
+  let mongoServer;
+
+  const scenario1 = {
+    _id: new mongoose.mongo.ObjectId("000000000000000000000001"),
+    name: "Scenario 1",
+    uid: "user1",
+  };
+
+  const mockRequest = (scenarioId, bodyContent) => ({
+    params: { scenarioId },
+    body: bodyContent,
+  });
+
+  const mockResponse = () => {
+    const res = {};
+    res.sendStatus = jest.fn().mockReturnValue(res);
+    return res;
+  };
+
+  const nextFunction = jest.fn();
+
+  // setup in-memory mongodb and express API
+  beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  });
+
+  beforeEach(async () => {
+    // Add scenario to database
+    await Scenario.create(scenario1);
+  });
+
+  // clear the database
+  afterEach(async () => {
+    await mongoose.connection.db.dropDatabase();
+  });
+
+  // close the mongodb and express servers
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+  });
+
+  it("successfully authorise user for scenario", async () => {
+    const req = mockRequest("000000000000000000000001", { uid: "user1" });
+    const res = mockResponse();
+    await scenarioAuth(req, res, nextFunction);
+
+    expect(nextFunction).toBeCalledTimes(1);
+  });
+
+  it("fails unauthorised user for scenario", async () => {
+    const req = mockRequest("000000000000000000000001", { uid: "user2" });
+    const res = mockResponse();
+    await scenarioAuth(req, res, nextFunction);
+    expect(res.sendStatus).toHaveBeenCalledWith(HTTP_UNAUTHORISED);
+  });
+
+  it("fails authorised user, invalid scenarioId", async () => {
+    const req = mockRequest("000000000000000000000002", { uid: "user1" });
+    const res = mockResponse();
+    await scenarioAuth(req, res, nextFunction);
+    expect(res.sendStatus).toHaveBeenCalledWith(HTTP_NOT_FOUND);
+  });
+});
