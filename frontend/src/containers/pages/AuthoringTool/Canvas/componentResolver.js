79d7c908dc29cb0efba62d4028691b156703a7b0
David Xiao <davidjinxiao@hotmail.com>
2021-09-25 12:41:04 +1200

    moved id generation to adding components

--- frontend/src/containers/pages/AuthoringTool/Canvas/componentResolver.js
+++ frontend/src/containers/pages/AuthoringTool/Canvas/componentResolver.js
@@ -1,14 +1,10 @@
 /* eslint-disable no-param-reassign */
 import React from "react";
-import { v4 } from "uuid";
 import ButtonComponent from "../Components/ButtonComponent";
 import TextComponent from "../Components/TextComponent";
 import ImageComponent from "../Components/ImageComponent";
 
 export default function componentResolver(component, index, onClick) {
-  if (component.id == null) {
-    component.id = v4();
-  }
   switch (component.type) {
     // ADD NEW COMPONENT TYPES HERE
     case "BUTTON":
