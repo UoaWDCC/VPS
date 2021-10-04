89cc40fea423dca69d2a014d6729ef6685014cef
flexzy <fzyang2000@hotmail.co.nz>
2021-10-05 00:07:59 +1300

    [VPS-114] Able to delete or decrement count of firebase image when saving a scene

---
+++ backend/src/firebase/firebase.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/prefer-default-export */
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const app = initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+});
+
+const storage = getStorage(app);
+
+export { storage };
