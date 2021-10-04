19ec3f46bb55446835c0b076f0c454a9eae6954d
David Xiao <davidjinxiao@hotmail.com>
2021-10-04 15:50:37 +1300

    [VPS-102] fixed text component newline bug

--- frontend/src/containers/pages/AuthoringTool/Components/components.styles.js
+++ frontend/src/containers/pages/AuthoringTool/Components/components.styles.js
@@ -19,6 +19,7 @@ export default function useStyles(component) {
       overflow: "hidden",
       color: component.color, // text colour
       textAlign: component.textAlign,
+      whiteSpace: "pre-wrap",
     },
   })();
 }
