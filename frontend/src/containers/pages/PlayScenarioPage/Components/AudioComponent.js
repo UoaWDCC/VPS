e68376f0d88a4d3427644b1aede9ffe745070bca
Lucas Gao <lucas2005gao@gmail.com>
2021-10-09 02:33:50 +1300

    [VPS-123] Linked the ReactAudioPlayer

--- frontend/src/containers/pages/PlayScenarioPage/Components/AudioComponent.js
+++ frontend/src/containers/pages/PlayScenarioPage/Components/AudioComponent.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactAudioPlayer from "react-audio-player";
 
-function AudioComponent({ key, id, onClick, component }) {
-  console.log(component);
-  return <ReactAudioPlayer src="my_audio_file.ogg" autoPlay />;
+function AudioComponent({ id, component }) {
+  return (
+    <ReactAudioPlayer
+      id={id}
+      src={`${component.url}`}
+      autoPlay
+      loop={!!component.loop}
+    />
+  );
 }
 
 export default AudioComponent;
