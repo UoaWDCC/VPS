94215c15b20da3420e5b44f81425d2e0cc9688ac
Lucas Gao <lucas2005gao@gmail.com>
2021-10-09 02:22:09 +1300

    [VPS-123] Added playing scenario componentResolver

---
+++ frontend/src/containers/pages/PlayScenarioPage/componentResolver.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-param-reassign */
+import React from "react";
+
+// Shared components from authoring canvas
+import ButtonComponent from "../AuthoringTool/Components/ButtonComponent";
+import TextComponent from "../AuthoringTool/Components/TextComponent";
+import ImageComponent from "../AuthoringTool/Components/ImageComponent";
+import FirebaseImageComponent from "../AuthoringTool/Components/FirebaseImageComponent";
+
+// Components for playing only
+import AudioComponent from "./Components/AudioComponent";
+
+export default function componentResolver(component, index, onClick) {
+  switch (component.type) {
+    // ADD NEW COMPONENT TYPES HERE
+    case "BUTTON":
+      return (
+        <ButtonComponent
+          key={component.id}
+          id={index}
+          onClick={onClick}
+          component={component}
+        />
+      );
+    case "TEXT":
+      return (
+        <TextComponent
+          key={component.id}
+          id={index}
+          onClick={onClick}
+          component={component}
+        />
+      );
+    case "IMAGE":
+      return (
+        <ImageComponent
+          key={component.id}
+          id={index}
+          onClick={onClick}
+          component={component}
+        />
+      );
+    case "FIREBASEIMAGE":
+      return (
+        <FirebaseImageComponent
+          key={component.id}
+          id={index}
+          onClick={onClick}
+          component={component}
+        />
+      );
+    case "FIREBASEAUDIO":
+      return (
+        <AudioComponent
+          key={component.id}
+          id={index}
+          onClick={onClick}
+          component={component}
+        />
+      );
+    default:
+      break;
+  }
+
+  return <div />;
+}
