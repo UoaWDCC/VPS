59e97a6bfa95026b55e80672034aa6de8a6cc4d7
lia-arroyo <liayzabel@gmail.com>
2022-05-29 02:28:40 +1200

    [VPS-022] added user field for scene timer duration in scene settings.

--- frontend/src/containers/pages/AuthoringTool/CanvasSideBar/SceneSettings.js
+++ frontend/src/containers/pages/AuthoringTool/CanvasSideBar/SceneSettings.js
@@ -1,12 +1,16 @@
 import React, { useContext } from "react";
 import TextField from "@material-ui/core/TextField";
 import { withStyles } from "@material-ui/core/styles";
+import { InputAdornment } from "@material-ui/core";
 import SceneContext from "../../../../context/SceneContext";
 
 import styles from "../../../../styling/CanvasSideBar.module.scss";
 
 const CustomTextField = withStyles({
   root: {
+    marginTop: "0.5em",
+    marginBottom: "1.5em",
+
     "& label.Mui-focused": {
       color: "#008a7b",
     },
@@ -28,6 +32,7 @@ export default function SceneSettings() {
       <div className={styles.sceneSettingsContainer}>
         <h1 className={styles.sideBarHeader}>Scene Settings</h1>
         <div className={styles.sideBarBody}>
+          {/* input for scene name */}
           <CustomTextField
             label="Scene Name"
             value={currentScene?.name}
@@ -37,6 +42,17 @@ export default function SceneSettings() {
                 ...currentScene,
                 name: event.target.value,
               });
+            }}
+          />
+          {/* input for scene timer duration */}
+          <CustomTextField
+            label="Scene Timer Duration"
+            type="number"
+            fullWidth
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">seconds</InputAdornment>
+              ),
             }}
           />
         </div>
