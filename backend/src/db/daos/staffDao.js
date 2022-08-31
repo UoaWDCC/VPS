c700e434d3491ebc4dfe80df3545ce5d3e81955a
lia-arroyo <liayzabel@gmail.com>
2022-08-31 12:42:43 +1200

    [VPS-029] route made for staff list collection

---
+++ backend/src/db/daos/staffDao.js
@@ -0,0 +1,12 @@
+import Staff from "../models/staff";
+
+/**
+ * Retrieves all the authorised staff stored in the database
+ * @returns list of database authorise staff objects
+ */
+ const retrieveAuthorisedStaffList = async () => {
+    return Staff.find();
+};
+
+export { retrieveAuthorisedStaffList };
+  
--- backend/src/routes/api/index.js
+++ backend/src/routes/api/index.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import scenario from "./scenario";
 import image from "./image";
+import staff from "./staff";
 
 const router = Router();
 
 router.use("/scenario", scenario);
 router.use("/image", image);
+router.use("/staff", staff);
 
 export default router;
