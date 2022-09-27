3d08ac8dcc597d140cb786041808664f47a9c566
retinfai <aferetipama@gmail.com>
2022-09-28 08:12:32 +1300

    [VPS-032] style graph node

--- frontend/src/containers/pages/Dashboard/SceneNode.js
+++ frontend/src/containers/pages/Dashboard/SceneNode.js
@@ -3,25 +3,12 @@ import { Handle, Position } from "react-flow-renderer";
 import { Box } from "@material-ui/core";
 import Thumbnail from "../../../components/Thumbnail";
 
-const boxStyle = {
-  backgroundColor: "#008A7B",
-  borderRadius: "5px",
-  padding: "1rem",
-  border: "3px solid black",
-};
-const textStyle = {
-  fontWeight: "normal",
-  border: "3px solid red",
-  margin: "0",
-  padding: "0",
-};
-
 function SceneNode({ data }) {
   const { scenarioId, sceneId, sceneTitle } = data;
   console.log(scenarioId, sceneId, sceneTitle);
 
   return (
-    <Box sx={boxStyle}>
+    <Box>
       <Handle type="target" position={Position.Top} />
       <Thumbnail
         url={`${process.env.PUBLIC_URL}/play/${scenarioId}/${sceneId}`}
@@ -29,7 +16,7 @@ function SceneNode({ data }) {
         height="90"
       />
       <Handle type="source" position={Position.Bottom} id="a" />
-      <h5 style={textStyle}>{sceneTitle}</h5>
+      {/* <h5 style={textStyle}>{sceneTitle}</h5> */}
     </Box>
   );
 }
