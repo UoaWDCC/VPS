dfb005f5e509ce69068eb9785ae80248f05e04f4
j-enn-y <127184310+j-enn-y@users.noreply.github.com>
2023-06-05 22:36:45 +1200

    Margin of pop-up set to 0.

--- frontend/src/components/TimerComponent/PopUp.js
+++ frontend/src/components/TimerComponent/PopUp.js
@@ -2,21 +2,22 @@ import * as React from "react";
 
 const style = {
   position: "absolute",
-  bottom: "-2%",
+  bottom: "0%",
   right: "0%",
   width: "20%",
   backgroundColor: "red",
   border: "2px solid #000",
+  margin: 0,
   padding: "40px",
 };
 
 function PopUp({ seconds }) {
   return (
-    <>
-      {seconds > 15 && <p style={style}>You have less than 30 seconds left!</p>}
-
-      {seconds < 16 && <p style={style}>You have less than 15 seconds left!</p>}
-    </>
+    <p style={style}>
+      {seconds > 15
+        ? "You have less than 30 seconds left!"
+        : "You have less than 15 seconds left!"}
+    </p>
   );
 }
 
