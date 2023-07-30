298394d2bc82cb636766cb5cdb6381ff6ac10263
Arnav Shekaran <78939786+arnard76@users.noreply.github.com>
2023-07-30 22:52:42 +1200

    change the styles of the speech box based on arrow location
    on box itself, change which corner is square (no border radius)
    
    on arrow, change rotation and alignment to move to `flex-start` or `flex-end`

--- frontend/src/containers/pages/AuthoringTool/Components/SpeechTextComponent.js
+++ frontend/src/containers/pages/AuthoringTool/Components/SpeechTextComponent.js
@@ -7,6 +7,26 @@ import useStyles from "./components.styles";
 const borderWidth = 3.429; // px
 
 /**
+ * @param {number} borderRadius
+ * @param {string} squareCorner
+ * @returns {string} borderRadius CSS value
+ */
+function formatRadius(borderRadius, squareCorner) {
+  const diffRadii = new Array(4).fill(`${borderRadius}px`);
+
+  const position = {
+    TopLeft: 0,
+    TopRight: 1,
+    BottomRight: 2,
+    BottomLeft: 3,
+  }[squareCorner];
+
+  diffRadii[position] = "0"; // this is the square corner
+
+  return diffRadii.join(" ");
+}
+
+/**
  * This React component represents a speech text scene component
  * @component
  * @example
@@ -20,16 +40,34 @@ export default function SpeechTextComponent({ id, onClick, component }) {
   const { defaultComponentStyling, speechTextComponentStyles } =
     useStyles(component);
   const [speechTextboxArrowWidth, setArrowWidth] = useState(null);
+
   const context = useContext(AuthoringToolContext);
   const textRef = context?.propertiesRefs?.text;
+
+  // style related values
+  const speechboxWidthOffset = 10;
+  const arrowLocation = component.arrowLocation || "right";
+  const arrowLocationStyles = {
+    top: { flexDirection: "column", squareCorner: "TopRight" },
+    bottom: {
+      flexDirection: "column-reverse",
+      squareCorner: "BottomLeft",
+    },
+    left: { flexDirection: "row", squareCorner: "TopLeft" },
+    right: {
+      flexDirection: "row-reverse",
+      squareCorner: "BottomRight",
+    },
+  }[arrowLocation];
+
   return (
     // eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events
     <div
       className={defaultComponentStyling}
       style={{
         display: "flex",
-        flexDirection: "column",
-        minWidth: speechTextboxArrowWidth + 10, // speechbox slightly wider than arrow
+        flexDirection: arrowLocationStyles.flexDirection,
+        minWidth: speechTextboxArrowWidth + speechboxWidthOffset, // speechbox slightly wider than arrow
       }}
       onClick={onClick}
       onDoubleClick={() => textRef?.current?.focus()}
@@ -38,14 +76,17 @@ export default function SpeechTextComponent({ id, onClick, component }) {
       <SpeechTextboxArrow
         borderWidth={borderWidth}
         setArrowWidth={setArrowWidth}
+        arrowLocation={arrowLocation}
       />
 
       <Typography
         style={{
           borderWidth,
           flex: 1,
-          borderRadius: "10px",
-          borderTopRightRadius: 0,
+          borderRadius: formatRadius(
+            speechboxWidthOffset,
+            arrowLocationStyles.squareCorner
+          ),
         }}
         className={speechTextComponentStyles}
       >
