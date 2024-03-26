a6a154c561f8389867ac667f15e8f08118977e63
xche529 <xche529@aucklanduni.ac.nz>
2024-03-27 12:38:18 +1300

    Fixed code style

--- frontend/src/components/__tests__/DashedCard.test.js
+++ frontend/src/components/__tests__/DashedCard.test.js
@@ -1,18 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import renderer from "react-test-renderer";
 import DashedCard from "../DashedCard";
-import TestRenderer from "react-test-renderer";
-const { act } = TestRenderer;
 
-let component;
 test("Dashed Card component snapshot test", () => {
-  act(() => {
-    component = renderer.create(
-      <BrowserRouter>
-        <DashedCard />
-      </BrowserRouter>
-    );
-  });
+  const component = renderer.create(
+    <BrowserRouter>
+      <DashedCard />
+    </BrowserRouter>
+  );
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
