bbc74421192dfbef5b30af7bc8b04da86d568098
xche529 <xche529@aucklanduni.ac.nz>
2024-03-27 02:01:39 +1300

    Added the plus sign

--- frontend/src/components/DashedCard.jsx
+++ frontend/src/components/DashedCard.jsx
@@ -17,21 +17,25 @@ import styles from "../styling/ListContainer.module.scss";
 export default function DashedCard({ onClick }) {
   return (
     <div className={styles.imageListItemWide}>
-      <Box
-        height={160}
-        border="5px dashed grey"
-        borderRadius={10}
-        borderColor="#747474"
-        overflow="hidden"
-        textAlign="center"
-        sx={{
-          background: "#f1f1f1",
-          "&:hover": {
-            background: "#cccccc",
-          },
-        }}
-        onClick={onClick}
-      />
+      <div style={{ position: "relative" }}>
+        <Box
+          height={160}
+          border="5px dashed grey"
+          borderRadius={10}
+          borderColor="#747474"
+          overflow="hidden"
+          textAlign="center"
+          sx={{
+            background: "#f1f1f1",
+            "&:hover": {
+              background: "#cccccc",
+            },
+          }}
+          onClick={onClick}
+        />
+        <div className={styles.crossHorizontalLine} />
+        <div className={styles.crossVerticalLine} />
+      </div>
       <p className={styles.text}>Create New Scene</p>
     </div>
   );
