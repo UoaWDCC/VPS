18cc27952836c5c4750904f394691376b98cc717
Kot <89110272+Kot6603@users.noreply.github.com>
2024-04-27 12:56:38 +1200

    [VPS-154] feat(groups): Added the database schema for groups
    [VPS-154] feat(groups): Added capabilities to add groups from a csv
    
    [VPS-154] feat(groups): added validation for creating groups
    
    [VPS-154] feat(groups): moved roleList to scenario

--- backend/src/routes/api/group.js
+++ backend/src/routes/api/group.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 
 import { addSceneToPath, getCurrentScene } from "../../db/daos/groupDao";
+import createGroup from "../../db/daos/groupDao";
+import { updateRoleList } from "../../db/daos/scenarioDao";
+import Group from "../../db/models/group";
 
 const router = Router();
 
 const HTTP_OK = 200;
 const HTTP_CONFLICT = 409;
 const HTTP_NOT_FOUND = 404;
+const HTTP_NO_CONTENT = 204;
 
 // add a scene to the group's shared path
 router.post("/path/:groupId", async (req, res) => {
@@ -43,6 +47,61 @@ router.get("/path/:groupId", async (req, res) => {
   } catch (error) {
     return res.status(HTTP_NOT_FOUND).json({ error: error.message });
   }
+});
+
+// create a new group
+router.post("/:scenarioId", async (req, res) => {
+  const { groupList, roleList } = req.body;
+  const { scenarioId } = req.params;
+
+  // check for scenario role list
+  // if it doesn't exist, create it
+  await updateRoleList(scenarioId, roleList);
+
+  // validation
+  groupList.forEach((userList) => {
+    if (!userList) {
+      res.status(HTTP_NO_CONTENT).send("No content");
+    }
+
+    const roles = [];
+    userList.forEach((user) => {
+      if (!user.email) {
+        res.status(HTTP_NO_CONTENT).send("No content");
+      }
+      if (!user.name) {
+        res.status(HTTP_NO_CONTENT).send("No content");
+      }
+      if (!user.role) {
+        res.status(HTTP_NO_CONTENT).send("No content");
+      }
+      if (!user.group) {
+        res.status(HTTP_NO_CONTENT).send("No content");
+      }
+
+      const role = user.role.toLowerCase();
+      if (roles.includes(role)) {
+        res.status(HTTP_CONFLICT).send("Conflict");
+      }
+      roles.push(role);
+    });
+
+    if (roles.length !== roleList.length) {
+      res.status(HTTP_CONFLICT).send("Conflict");
+    }
+  });
+
+  await Group.deleteMany({ scenarioId });
+
+  const output = [];
+  const promises = groupList.map(async (userList) => {
+    const group = await createGroup(scenarioId, userList);
+    output.push(group);
+  });
+
+  await Promise.all(promises);
+
+  res.status(HTTP_OK).json(output);
 });
 
 export default router;
