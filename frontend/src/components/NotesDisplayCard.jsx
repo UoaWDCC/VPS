8a86a8938ee0426d89a193d0909965d29c827084
xche529 <xche529@aucklanduni.ac.nz>
2024-05-06 16:47:01 +1200

    feat: add display board for notes

--- frontend/src/components/NotesDisplayCard.jsx
+++ frontend/src/components/NotesDisplayCard.jsx
@@ -1,12 +1,52 @@
-import React from "react";
+import { React, useState } from "react";
 import styles from "../styling/NotesDisplayCard.module.scss";
 
 export default function NotesDisplayCard() {
+  const [name, setName] = useState("default name");
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Escape") {
+      handleClose();
+    }
+  };
+
   return (
     <>
-      <button type="button" className={styles.notesButton}>
+      <button
+        type="button"
+        className={styles.notesButton}
+        onClick={() => {
+          handleOpen();
+        }}
+      >
         Notes
       </button>
+      {open && (
+        <div>
+          <div
+            className={styles.overlay}
+            role="button"
+            tabIndex={0}
+            onClick={handleClose}
+            onKeyDown={handleKeyPress}
+            aria-label="Close Create Scenario Card"
+          />
+
+          <div className={styles.noteCard}>
+            {" "}
+            <h1>Display notes here</h1>
+          </div>
+        </div>
+      )}
     </>
   );
 }
