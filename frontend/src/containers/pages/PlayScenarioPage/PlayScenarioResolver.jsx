ae3b15d5ea64444057dd1d0bab49dfe9c31e42c8
harbassan <harbassan@hotmail.com>
2024-05-14 00:02:36 +1200

    feat(frontend/PlayScenarioPage): add multiplayer component and route resolver

---
+++ frontend/src/containers/pages/PlayScenarioPage/PlayScenarioResolver.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useContext, useState } from "react";
+import {
+  Route,
+  Switch,
+  useHistory,
+  useLocation,
+  useParams,
+} from "react-router-dom";
+import axios from "axios";
+
+import AuthenticationContext from "context/AuthenticationContext";
+import useGraph from "hooks/useGraph";
+
+import PlayScenarioPage from "./PlayScenarioPage";
+import PlayScenarioPageMulti from "./PlayScenarioPageMulti";
+
+// TODO: move this somewhere else and add error handling
+async function get(url, userIdToken) {
+  const config = userIdToken && {
+    headers: { Authorization: `Bearer ${userIdToken()}` },
+  };
+
+  return axios.get(url, config);
+}
+
+/**
+ * This resolver fetches the necessary scenario data and redirects users to the correct page
+ */
+export default function PlayScenarioResolver() {
+  const { scenarioId } = useParams();
+  const location = useLocation();
+  const history = useHistory();
+
+  const { graph } = useGraph(scenarioId);
+  const { user, getUserIdToken: token } = useContext(AuthenticationContext);
+  const [group, setGroup] = useState(null);
+
+  const isMain = location.pathname === `/play/${scenarioId}`;
+  const isMulti = location.pathname.includes("multiplayer");
+
+  useEffect(async () => {
+    if (!(graph && (isMain || (isMulti && !group)))) return;
+
+    const res = await get(`/api/user/${user.uid}/${scenarioId}/group`, token);
+    const sceneId = res?.data?.group?.current || graph.getScenes()[0]._id;
+    const multiplayerPath = res?.data?.group && "multiplayer";
+
+    setGroup(res?.data?.group || "none");
+    history.replace(`/play/${scenarioId}/${multiplayerPath}/${sceneId}`);
+  }, [graph, isMain, isMulti, scenarioId]);
+
+  return (
+    <Switch>
+      <Route path="/play/:scenarioId/multiplayer/:sceneId">
+        <PlayScenarioPageMulti graph={graph} group={group} />
+      </Route>
+      <Route path="/play/:scenarioId/:sceneId">
+        <PlayScenarioPage graph={graph} />
+      </Route>
+    </Switch>
+  );
+}
--- frontend/src/context/PlayScenarioContext.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const PlayScenarioContext = React.createContext();
-
-export default PlayScenarioContext;
