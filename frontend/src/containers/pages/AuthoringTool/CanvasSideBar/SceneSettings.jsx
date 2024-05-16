55ea3bd1ea9100358167f28c49f2e1e39c66a37f
atulbox <kodlaatul@gmail.com>
2024-05-16 22:17:31 +1200

    feat: create a dropdown checkbox menu

--- frontend/src/containers/pages/AuthoringTool/CanvasSideBar/SceneSettings.jsx
+++ frontend/src/containers/pages/AuthoringTool/CanvasSideBar/SceneSettings.jsx
@@ -1,11 +1,21 @@
-import { InputAdornment } from "@material-ui/core";
+import {
+  InputLabel,
+  InputAdornment,
+  FormControl,
+  FormControlLabel,
+  Select,
+  Checkbox,
+  Box,
+} from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
 import { withStyles } from "@material-ui/core/styles";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import SceneContext from "../../../../context/SceneContext";
 
 import styles from "../../../../styling/CanvasSideBar.module.scss";
+import CustomInputLabelStyles from "./CustomPropertyInputStyles/CustomInputLabelStyles";
 
+const CustomInputLabel = CustomInputLabelStyles()(InputLabel);
 const CustomTextField = withStyles({
   root: {
     marginTop: "0.5em",
@@ -26,6 +36,25 @@ const CustomTextField = withStyles({
  */
 export default function SceneSettings() {
   const { currentScene, setCurrentScene } = useContext(SceneContext);
+  const [checked, setChecked] = useState([true, true, true]);
+  const [allChecked, setAllChecked] = useState([true]);
+
+  const handleCheckboxChange = (index) => {
+    const newChecked = [...checked];
+    newChecked[index] = !checked[index];
+    setChecked(newChecked);
+
+    const isAllChecked = newChecked.every((isChecked) => isChecked);
+    setAllChecked(isAllChecked);
+  };
+
+  const handleAllToggle = () => {
+    const newAllChecked = !allChecked;
+    setAllChecked(newAllChecked);
+
+    const newChecked = allChecked ? [false, false, false] : [true, true, true];
+    setChecked(newChecked);
+  };
 
   return (
     <>
@@ -70,6 +99,53 @@ export default function SceneSettings() {
               shrink: !!currentScene?.time,
             }}
           />
+          <FormControl fullWidth>
+            <CustomInputLabel>Scene Role(s)</CustomInputLabel>
+            <Select className={styles.selectInput}>
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      defaultChecked
+                      checked={allChecked}
+                      onChange={handleAllToggle}
+                    />
+                  }
+                  label="All"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      defaultChecked
+                      checked={checked[0]}
+                      onChange={() => handleCheckboxChange(0)}
+                    />
+                  }
+                  label="Doctor"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      defaultChecked
+                      checked={checked[1]}
+                      onChange={() => handleCheckboxChange(1)}
+                    />
+                  }
+                  label="Nurse"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      defaultChecked
+                      checked={checked[2]}
+                      onChange={() => handleCheckboxChange(2)}
+                    />
+                  }
+                  label="Pharmacist"
+                />
+              </div>
+            </Select>
+          </FormControl>
         </div>
       </div>
     </>
