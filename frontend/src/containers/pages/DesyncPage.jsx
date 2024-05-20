55486f76943e8e9bde5fa86ca42ef327d402994a
Subeen Ban <sban919@aucklanduni.ac.nz>
2024-05-20 14:19:21 +1200

    fixed desync page

--- frontend/src/containers/pages/DesyncPage.jsx
+++ frontend/src/containers/pages/DesyncPage.jsx
@@ -36,15 +36,21 @@ export default function DesyncPage({ group }) {
       </Grid>
       <Grid item container direction="column" alignItems="center" spacing={2}>
         <Grid item>
-          <Typography variant="h4">Desynchronized with Server</Typography>
+          <Typography variant="h4">Desynchronised with server!</Typography>
         </Grid>
         <Grid>
-          <Typography>This can happen asdlfkaldkfj</Typography>
+          <Typography>
+            This can happen when someone else made a move while you were
+            playing.
+          </Typography>
+        </Grid>
+        <Grid>
+          <Typography>Your last action was not saved.</Typography>
         </Grid>
       </Grid>
       <Grid item>
         <Button color="primary" variant="contained" onClick={onBack}>
-          Back
+          Back to Scenario
         </Button>
       </Grid>
     </Grid>
