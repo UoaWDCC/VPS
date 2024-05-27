a7b05e266ce041a5155fc36634658f8e777a0427
atulbox <kodlaatul@gmail.com>
2024-05-27 18:59:40 +1200

    fix(AuthoringToolPage): remove tag component and associated code

--- frontend/src/containers/pages/AuthoringTool/SceneNavigator/SceneListItem.jsx
+++ frontend/src/containers/pages/AuthoringTool/SceneNavigator/SceneListItem.jsx
@@ -2,60 +2,8 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 
-import { useRef } from "react";
-import styles from "styling/SceneNavigator.module.scss";
-import { noTagSymbol, tagColours, tagOptions } from "./SceneTagCustomization";
-
-const SceneListItem = ({
-  sceneId,
-  tag,
-  thumbnail,
-  showingTagInputFor,
-  showTagInputFor,
-  selectTagEl,
-}) => {
-  const tagRef = useRef(null);
-  const fullTagRef = useRef(null);
-
-  function toggleTagOnHover() {
-    const tagEl = tagRef.current;
-    const fullTagEl = fullTagRef.current;
-
-    tagEl.classList.toggle(styles.hidden);
-    fullTagEl.classList.toggle(styles.showing);
-  }
-
-  function toggleInputBox() {
-    showTagInputFor(showingTagInputFor === sceneId ? null : sceneId);
-    selectTagEl(showingTagInputFor === sceneId ? null : tagRef.current);
-  }
-
-  const noTagClassName = tag === noTagSymbol ? styles.noTag : "";
-
-  return (
-    <li key={sceneId}>
-      {thumbnail}
-      <p
-        className={`${noTagClassName} ${styles.playersTag}`}
-        style={{ borderColor: tagColours[tag] || "black" }}
-        onMouseEnter={() => toggleTagOnHover(sceneId)}
-        onClick={() => toggleInputBox(sceneId)}
-        ref={tagRef}
-      >
-        {tag}
-      </p>
-
-      <p
-        className={`${noTagClassName} ${styles.playersTagFull}`}
-        style={{ borderColor: tagColours[tag] || "black" }}
-        onMouseLeave={() => toggleTagOnHover(sceneId)}
-        onClick={() => toggleInputBox(sceneId)}
-        ref={fullTagRef}
-      >
-        {tagOptions[tag]}
-      </p>
-    </li>
-  );
+const SceneListItem = ({ sceneId, thumbnail }) => {
+  return <li key={sceneId}>{thumbnail}</li>;
 };
 
 export default SceneListItem;
--- frontend/src/containers/pages/AuthoringTool/SceneNavigator/SceneNavigator.jsx
+++ frontend/src/containers/pages/AuthoringTool/SceneNavigator/SceneNavigator.jsx
@@ -4,45 +4,19 @@ import { useContext, useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import styles from "styling/SceneNavigator.module.scss";
 import SceneListItem from "./SceneListItem";
-import SceneTagInput from "./SceneTagInput";
 
 const SceneNavigator = ({ saveScene }) => {
   const [thumbnails, setThumbnails] = useState(null);
-  const { scenes, setScenes, currentScene, currentSceneRef, setCurrentScene } =
+  const { scenes, currentScene, currentSceneRef, setCurrentScene } =
     useContext(SceneContext);
   const { scenarioId } = useParams();
   const history = useHistory();
-
-  // showing tag modal
-
-  const [showingTagInputFor, showTagInputFor] = useState(null);
-
-  function getIndexOfSceneId(sceneId) {
-    if (!thumbnails) return -1;
-    return thumbnails.findIndex((thumbnail) => thumbnail.sceneId === sceneId);
-  }
-
-  function changeTag(sceneId, newTag) {
-    const sceneIndex = getIndexOfSceneId(sceneId);
-
-    setThumbnails((currentThumbnails) => {
-      currentThumbnails[sceneIndex].tag = newTag;
-      return [...currentThumbnails];
-    });
-
-    setScenes((currScenes) => {
-      const index = currScenes.findIndex((scene) => scene._id === sceneId);
-      currScenes[index].tag = newTag;
-      return currScenes;
-    });
-  }
 
   useEffect(() => {
     if (scenes !== undefined) {
       setThumbnails(
         scenes.map((scene, index) => ({
           sceneId: scene._id,
-          tag: scene.tag,
           sceneListItem: (
             <>
               <p
@@ -86,38 +60,20 @@ const SceneNavigator = ({ saveScene }) => {
     }
   }, [scenes, currentScene]);
 
-  // for the scene/tag inputs
-  const [selectedTagEl, selectTagEl] = useState(null);
-
   return (
     thumbnails && (
       <div style={{ display: "flex", position: "relative" }}>
         <ul className={styles.sceneNavigator}>
-          {thumbnails.map(({ sceneListItem: thumbnail, sceneId, tag }) => (
+          {thumbnails.map(({ sceneListItem: thumbnail, sceneId }) => (
             <SceneListItem
               {...{
                 thumbnail,
                 sceneId,
-                tag,
-                showingTagInputFor,
-                showTagInputFor,
-                selectTagEl,
                 key: sceneId,
               }}
             />
           ))}
         </ul>
-
-        {showingTagInputFor && (
-          <SceneTagInput
-            {...{
-              selectedTagEl,
-              changeTag,
-              showTagInputFor,
-              showingTagInputFor,
-            }}
-          />
-        )}
       </div>
     )
   );
