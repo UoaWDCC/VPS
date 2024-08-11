b9745b002e2c3bf1498bd5ef37371d08e582b670
Kot <89110272+Kot6603@users.noreply.github.com>
2024-08-12 02:00:48 +1200

    [VPS-32]: feat: added a flag property onto the button components (UI)

--- frontend/src/containers/pages/AuthoringTool/CanvasSideBar/ComponentProperties/ButtonPropertiesComponent.jsx
+++ frontend/src/containers/pages/AuthoringTool/CanvasSideBar/ComponentProperties/ButtonPropertiesComponent.jsx
@@ -1,19 +1,24 @@
 import {
+  Button,
+  Checkbox,
   FormControl,
+  FormControlLabel,
   InputLabel,
   MenuItem,
   Select,
   TextField,
 } from "@material-ui/core";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import SceneContext from "../../../../../context/SceneContext";
 import CustomInputLabelStyles from "../CustomPropertyInputStyles/CustomInputLabelStyles";
 import CustomTextFieldStyles from "../CustomPropertyInputStyles/CustomTextFieldStyles";
+import CustomCheckBoxStyles from "../CustomPropertyInputStyles/CustomCheckBoxStyles";
 
 import styles from "../../../../../styling/CanvasSideBar.module.scss";
 
 const CustomTextField = CustomTextFieldStyles()(TextField);
 const CustomInputLabel = CustomInputLabelStyles()(InputLabel);
+const CustomCheckBox = CustomCheckBoxStyles()(Checkbox);
 
 /**
  * This component displays the properties in the sidebar for a button scene component.
@@ -24,6 +29,46 @@ export default function ButtonPropertiesComponent({
   componentIndex,
 }) {
   const { scenes, updateComponentProperty } = useContext(SceneContext);
+
+  const [newFlag, setNewFlag] = useState("New Flag");
+  const [flagList, setFlags] = useState(
+    () =>
+      component.flags || [
+        "add flag_1",
+        "remove flag_1",
+        "add flag_2",
+        "remove flag_2",
+      ]
+  );
+  const [checked, setChecked] = useState(
+    () => component.flagStates || [false, false, false, false]
+  );
+
+  const handleInputChange = (event) => {
+    setNewFlag(event.target.value);
+  };
+
+  const handleAddFlag = () => {
+    if (newFlag.trim() !== "") {
+      const newFlagList = [...flagList, `add ${newFlag}`, `remove ${newFlag}`];
+      setFlags(newFlagList);
+      setChecked([...checked, false, false]);
+      setNewFlag("");
+      updateComponentProperty(componentIndex, "flags", newFlagList);
+    }
+  };
+
+  const handleCheckboxChange = (index) => {
+    const newChecked = [...checked];
+    newChecked[index] = !checked[index];
+    setChecked(newChecked);
+    updateComponentProperty(componentIndex, "flagStates", newChecked);
+
+    // TODO:
+    // Need to implement the functionality when
+    // a checkbox is checked, the labelled flag should be
+    // added to the groups active flag list
+  };
 
   return (
     <>
@@ -96,6 +141,43 @@ export default function ButtonPropertiesComponent({
           })}
         </Select>
       </FormControl>
+
+      <FormControl fullWidth className={styles.componentProperty}>
+        <div style={{ display: "flex" }}>
+          <CustomTextField
+            label="New Flag"
+            value={newFlag}
+            fullWidth
+            onChange={handleInputChange}
+          />
+          <Button onClick={handleAddFlag}>Add</Button>
+        </div>
+
+        <Select
+          style={{ marginTop: "10px" }}
+          multiple
+          className={styles.selectInput}
+          value={flagList}
+          renderValue={(selected) => selected.join(", ")}
+          displayEmpty
+        >
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            {flagList.map((flag, index) => (
+              <FormControlLabel
+                key={componentIndex + flag}
+                control={
+                  <CustomCheckBox
+                    checked={checked[index] || false}
+                    onChange={() => handleCheckboxChange(index)}
+                  />
+                }
+                label={flag}
+              />
+            ))}
+          </div>
+        </Select>
+      </FormControl>
+
       <FormControl fullWidth className={styles.componentProperty}>
         <CustomTextField
           label="Z Axis Position"
