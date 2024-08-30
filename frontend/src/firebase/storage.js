5ffec5373c481a0a71d6f6d0f0e39e44104189a0
Kot <89110272+Kot6603@users.noreply.github.com>
2024-08-31 11:13:00 +1200

    temp: added state checking to image uploads

--- frontend/src/firebase/storage.js
+++ frontend/src/firebase/storage.js
@@ -22,7 +22,34 @@ import { storage } from "./firebase";
 const uploadFile = async (file, scenarioId, sceneId, isEndImage) => {
   const fileUUID = isEndImage ? "endImage" : v4();
   const storageRef = ref(storage, `${scenarioId}/${sceneId}/${fileUUID}`);
-  const uploadTask = await uploadBytesResumable(storageRef, file);
+  const uploadTask = uploadBytesResumable(storageRef, file);
+
+  uploadTask.on(
+    "state_changed",
+    (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log(`Upload is ${progress}% done`);
+      switch (snapshot.state) {
+        case "paused":
+          console.log("Upload is paused");
+          break;
+        case "running":
+          console.log("Upload is running");
+          break;
+        default:
+          break;
+      }
+    },
+    (error) => {
+      console.log("Error in uploading file:", error);
+    },
+    () => {
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        console.log('file available at ', downloadURL);
+      });
+    }
+  );
+
   const url = await getDownloadURL(uploadTask.ref);
   const metaData = {
     customMetadata: {
