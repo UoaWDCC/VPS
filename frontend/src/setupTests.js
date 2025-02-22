3cbbfe3340aec998b860ac4490b0782c4afa2a63
harbassan <harbassan@hotmail.com>
2025-02-23 00:34:39 +1300

    chore(frontend): remove test structure remnants

--- frontend/src/setupTests.js
+++
@@ -1,6 +0,0 @@
-/**
- * This file is automatically ran by React when running tests
- */
-
-// Mock firebase provides mock versions of Firebase libraries that is used during authentication
-import "./__mocks__/firebase";
