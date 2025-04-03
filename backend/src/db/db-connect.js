0cd77a77d66efd7be46eb894efcd860947753a8b
harbassan <harbassan@hotmail.com>
2025-04-04 00:48:49 +1300

    chore: switch atlas cluster

--- backend/src/db/db-connect.js
+++ backend/src/db/db-connect.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const DEFAULT_CONNECTION_STRING = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.ly1k5.mongodb.net/VPS?retryWrites=true&w=majority`;
+const DEFAULT_CONNECTION_STRING = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@primary.rjlaw.mongodb.net/primary?retryWrites=true&w=majority&appName=Primary`;
 
 /**
  * This function begins the process of connecting to the database, and returns a promise that will
