34b8df1c701e02812d332ccb057423d8265acb5f
Rayen Lee <rlee449@aucklanduni.ac.nz>
2025-05-08 21:58:45 +1200

    Updated error messaging in manage groups page
    Should hopefully be more information for users now

--- frontend/src/features/groups/ManageGroupsPage.jsx
+++ frontend/src/features/groups/ManageGroupsPage.jsx
@@ -107,8 +107,8 @@ export default function ManageGroupsPage() {
           console.log("CSV data uploaded to MongoDB:", response.status);
           showToast("Successfully formed groups!");
         } catch (error) {
-          console.error("Error uploading CSV data:", error);
-          showToast("Error uploading CSV data!", "error");
+          console.error("Error uploading CSV data:", error.response.data);
+          showToast(`Error uploading CSV data: ${error.response.data}`, "error");
         }
       },
     });
