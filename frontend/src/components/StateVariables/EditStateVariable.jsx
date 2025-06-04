aeca1b702048489fd86c0c92b1716fff3d7532d0
Rayen Lee <rlee449@aucklanduni.ac.nz>
2025-06-05 11:49:17 +1200

    EDIT State Variable Backend
    Also added an indicator for when a state variable is being edited (orangy-yellow border colour) and a reset button to revert back to the original value (this is purely a visual frontend thing)

--- frontend/src/components/StateVariables/EditStateVariable.jsx
+++ frontend/src/components/StateVariables/EditStateVariable.jsx
@@ -1,4 +1,5 @@
 import { Box, Grid, IconButton, Tooltip, Card } from "@material-ui/core";
+import ReplayIcon from "@mui/icons-material/Replay";
 import SaveIcon from "@material-ui/icons/Save";
 import DeleteIcon from "@material-ui/icons/Delete";
 import { api } from "../../util/api";
@@ -19,6 +20,35 @@ const EditStateVariable = ({
   const [newType, setNewType] = useState(type);
   const [newValue, setNewValue] = useState(value);
 
+  const editing = name !== newName || type !== newType || value != newValue;
+
+  const resetFields = () => {
+    setNewName(name);
+    setNewType(type);
+    setNewValue(value);
+  };
+
+  const editStateVariable = () => {
+    const newStateVariable = {
+      name: newName,
+      type: newType,
+      value: newValue,
+    };
+    api
+      .put(user, `api/scenario/${scenarioId}/stateVariables`, {
+        originalName: name,
+        newStateVariable,
+      })
+      .then((res) => {
+        setStateVariables(res.data);
+        toast.success("State variable edited!");
+      })
+      .catch((error) => {
+        console.error("Error editing state variable:", error);
+        toast.error("Failed to edit state variable.");
+      });
+  };
+
   const deleteStateVariable = () => {
     api
       .delete(user, `api/scenario/${scenarioId}/stateVariables/${name}`)
@@ -33,7 +63,14 @@ const EditStateVariable = ({
   };
 
   return (
-    <Card variant="outlined" style={{ padding: "12px 25px", marginBottom: 12 }}>
+    <Card
+      variant="outlined"
+      style={{
+        padding: "12px 25px",
+        marginBottom: 12,
+        ...(editing && { borderColor: "#ffa600" }),
+      }}
+    >
       <Grid container alignItems="center" spacing={2}>
         <StateVariableForm
           name={newName}
@@ -44,8 +81,13 @@ const EditStateVariable = ({
           setValue={setNewValue}
         />
         <Box ml="auto" display="flex" alignItems="center">
+          <Tooltip title="Reset">
+            <IconButton color="default" onClick={resetFields}>
+              <ReplayIcon />
+            </IconButton>
+          </Tooltip>
           <Tooltip title="Save">
-            <IconButton color="primary" onClick={() => {}}>
+            <IconButton color="primary" onClick={editStateVariable}>
               <SaveIcon />
             </IconButton>
           </Tooltip>
