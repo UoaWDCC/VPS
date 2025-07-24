badff19ab495e975859a7e824765337ecac60f7b
Rayen Lee <159082701+RLee64@users.noreply.github.com>
2025-07-24 17:08:03 +1200

    Create and Edit State Operations (#298)
    This implements a basic solution by adding the state operation menu to button properties. Similar to the state variable menu, authors are able to create state variables and edit them at a later point.
    
    ``StateOperationMenu.jsx`` - Houses the components for creating and editing state operations
    ``CreateStateOperation.jsx`` - Allows users to create state operations
    ``EditStateOperation.jsx`` - Allows users to edit state operations
    ``StateOperationForm.jsx`` - Contains form fields that used by both ``CreateStateOperation.jsx`` and ``EditStateOperation.jsx``
    ``Operations.jsx`` - Contains an array of valid state operations for each state type
    ``EditingTooltips.jsx`` - Contains reset, save, and delete tooltips used by both ``EditStateOperation.jsx`` and ``EditStateVariables.jsx``

---
+++ frontend/src/components/StateVariables/EditStateOperation.jsx
@@ -0,0 +1,99 @@
+import { Box, FormGroup, Typography } from "@material-ui/core";
+import { useContext, useState } from "react";
+import EditingTooltips from "./EditingTooltips";
+import StateOperationForm from "./StateOperationForm";
+import ScenarioContext from "../../context/ScenarioContext";
+import SceneContext from "../../context/SceneContext";
+
+/**
+ * Component used for editing state operations
+ * State operations are used to manipulate state variables while playing through a scenario
+ *
+ * @component
+ */
+const EditStateOperation = ({
+  componentIndex,
+  component,
+  operationIndex,
+  stateOperation,
+}) => {
+  const { stateVariables } = useContext(ScenarioContext);
+  const { updateComponentProperty } = useContext(SceneContext);
+
+  const stateVariable = stateVariables.find(
+    (variable) => variable.name === stateOperation.name
+  );
+
+  const [newOperation, setNewOperation] = useState(stateOperation.operation);
+  const [newValue, setNewValue] = useState(stateOperation.value);
+
+  const editing =
+    newOperation !== stateOperation.operation ||
+    newValue !== stateOperation.value;
+
+  const resetStateOperation = () => {
+    setNewOperation(stateOperation.operation);
+    setNewValue(stateOperation.value);
+  };
+
+  const editStateOperation = () => {
+    const newStateOperations = component.stateOperations.map(
+      (operation, index) =>
+        index === operationIndex
+          ? { ...operation, operation: newOperation, value: newValue }
+          : operation
+    );
+    updateComponentProperty(
+      componentIndex,
+      "stateOperations",
+      newStateOperations
+    );
+  };
+
+  const deleteStateOperation = () => {
+    const newStateOperations = component.stateOperations.filter(
+      (_, index) => index !== operationIndex
+    );
+    updateComponentProperty(
+      componentIndex,
+      "stateOperations",
+      newStateOperations
+    );
+  };
+
+  return (
+    <FormGroup
+      style={{
+        marginBottom: "30px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+        background: "#f9f9f9",
+        padding: "16px",
+        borderRadius: "8px",
+        border: "2px solid",
+        borderColor: editing ? "#ffa600" : "transparent",
+      }}
+    >
+      <Typography variant="subtitle1" fontWeight="bold">
+        {stateOperation.name}
+      </Typography>
+      <StateOperationForm
+        selectedState={stateVariable}
+        operation={newOperation}
+        setOperation={setNewOperation}
+        value={newValue}
+        setValue={setNewValue}
+      />
+      <Box width="100%" display="flex" justifyContent="space-between">
+        <EditingTooltips
+          onReset={resetStateOperation}
+          onSave={editStateOperation}
+          onDelete={deleteStateOperation}
+        />
+      </Box>
+    </FormGroup>
+  );
+};
+
+export default EditStateOperation;
---
+++ frontend/src/components/StateVariables/StateOperationForm.jsx
@@ -0,0 +1,84 @@
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import { validOperations } from "./Operations";
+import { StateTypes } from "./StateTypes";
+
+/**
+ * Component with fields shared in both creating and editing state operations
+ *
+ * @component
+ */
+const StateOperationForm = ({
+  selectedState,
+  operation,
+  setOperation,
+  value,
+  setValue,
+}) => {
+  if (!selectedState) {
+    return null;
+  }
+
+  return (
+    <>
+      <Typography variant="body2">
+        Type:{" "}
+        {selectedState.type.charAt(0).toUpperCase() +
+          selectedState.type.slice(1)}
+      </Typography>
+      <FormControl>
+        <InputLabel>Operation</InputLabel>
+        <Select
+          value={operation}
+          onChange={(e) => {
+            setOperation(e.target.value);
+          }}
+          required
+        >
+          {validOperations[selectedState.type].map((validOperation) => (
+            <MenuItem key={validOperation} value={validOperation}>
+              {validOperation.charAt(0).toUpperCase() + validOperation.slice(1)}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <FormControl>
+        {selectedState.type === StateTypes.BOOLEAN ? (
+          <>
+            <InputLabel>Value</InputLabel>
+            <Select
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              required
+            >
+              <MenuItem value={true}>True</MenuItem>
+              <MenuItem value={false}>False</MenuItem>
+            </Select>
+          </>
+        ) : (
+          <TextField
+            value={value}
+            label={`Value`}
+            onChange={(e) =>
+              setValue(
+                selectedState.type === StateTypes.NUMBER
+                  ? Number(e.target.value)
+                  : e.target.value
+              )
+            }
+            required
+            type={selectedState.type === StateTypes.NUMBER ? "number" : "text"}
+          />
+        )}
+      </FormControl>
+    </>
+  );
+};
+
+export default StateOperationForm;
