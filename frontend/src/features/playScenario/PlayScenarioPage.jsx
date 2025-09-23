252f5c33ed0a1270f2e6336a4bc043ce8be19fab
Rayen Lee <rlee449@aucklanduni.ac.nz>
2025-09-23 16:45:07 +1200

    delete debug stuff

--- frontend/src/features/playScenario/PlayScenarioPage.jsx
+++ frontend/src/features/playScenario/PlayScenarioPage.jsx
@@ -59,14 +59,6 @@ export default function PlayScenarioPage() {
 
   const currScene = sceneCache.get(sceneId);
 
-  useEffect(() => {
-    console.log(stateVariables);
-  }, [stateVariables]);
-
-  useEffect(() => {
-    console.log(stateVersion);
-  }, [stateVersion]);
-
   const handleError = (error) => {
     if (!error) return;
     if (error.status === 409) {
