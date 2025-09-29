4538e42acb5e2545c88ab6044e3c49d10efe75b7
harbassan <harbassan@hotmail.com>
2025-09-30 00:11:56 +1300

    fix: reintegrate existing image picker

---
+++ frontend/src/features/authoring/audio/UploadAudio.jsx
@@ -0,0 +1,51 @@
+// import { MenuItem } from "@material-ui/core";
+// import AttachFileIcon from "@material-ui/icons/AttachFile";
+// import Tooltip from "@material-ui/core/Tooltip";
+// import { useContext, useRef } from "react";
+// import SceneContext from "context/SceneContext";
+// import ToolbarContext from "context/ToolbarContext";
+// import styles from "./ToolBar.module.scss";
+// import { addFirebaseAudio } from "./ToolBarActions";
+//
+// /**
+//  * This component is a dropdown item for the audio button in the ToolBar.
+//  * It allows the user to upload mp3 files.
+//  * @component
+//  */
+// function UploadAudio() {
+//   const { currentScene, setCurrentScene } = useContext(SceneContext);
+//   const { handleDropdownClose } = useContext(ToolbarContext);
+//   const inputFile = useRef(null);
+//
+//   const handleFileInput = async (e) => {
+//     const url = await URL.createObjectURL(e.target.files[0]);
+//     addFirebaseAudio(currentScene, setCurrentScene, e.target.files[0], url);
+//     inputFile.current.value = null;
+//     handleDropdownClose();
+//   };
+//
+//   return (
+//     <div>
+//       <input
+//         type="file"
+//         accept=".mp3"
+//         ref={inputFile}
+//         style={{ display: "none" }}
+//         onChange={handleFileInput}
+//       />
+//       <Tooltip title="Upload an audio file" placement="right" arrow>
+//         <MenuItem
+//           className={styles.menuItem}
+//           color="default"
+//           variant="contained"
+//           onClick={() => inputFile.current.click()}
+//         >
+//           <AttachFileIcon />
+//           &nbsp;&nbsp;Upload Audio
+//         </MenuItem>
+//       </Tooltip>
+//     </div>
+//   );
+// }
+//
+// export default UploadAudio;
