08eff917be627e5ae564f4e358858742a0085400
Bradley Chung <badleyschool955@gmail.com>
2025-10-09 01:00:29 +1300

    Refactor dashboard
    Dashboard:
     - Now shows the graph on the main page
     - Now uses nested routes to display the main dashboard or group contents
    
    Misc:
     - Refactored code
     - Reorganized components

--- frontend/src/features/dashboard/Dashboard.jsx
+++ frontend/src/features/dashboard/Dashboard.jsx
@@ -1,21 +1,18 @@
 import ScreenContainer from "../../components/ScreenContainer/ScreenContainer";
 import { useGet } from "hooks/crudHooks";
-import { useParams, useHistory } from "react-router-dom";
-import { useState, useContext, useMemo } from "react";
-import ScenarioContext from "../../context/ScenarioContext";
+import { useParams, useHistory, useRouteMatch, Switch } from "react-router-dom";
+import { useEffect, useState, useMemo } from "react";
 import DashTopBar from "./components/DashTopBar";
 import HelpButton from "../../components/HelpButton";
 import DashGroupTable from "./components/DashGroupTable";
 import GroupsIcon from "@mui/icons-material/Groups";
-import BlockIcon from "@mui/icons-material/Block";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import HourglassEmptyIcon from "@mui/icons-material/HourglassEmpty";
-import SceneContext from "../../context/SceneContext";
 import { ReactFlowProvider } from "@xyflow/react";
-import CreateGraphData from "./components/GraphHelper";
+import CreateGraphData from "./utils/GraphHelper";
 import ScenarioGraph from "./components/ScenarioGraph";
-import GraphWrapper from "./components/GraphWrapper";
+import ProtectedRoute from "../../firebase/ProtectedRoute";
+import ViewGroup from "./components/ViewGroup";
 /**
  * Could maybe add some info about the scenario? Who created what time, last edited, thumbnail of the scenario and an overlay edit button * which directs you to the edit page?
  *
@@ -23,24 +20,61 @@ import GraphWrapper from "./components/GraphWrapper";
  */
 
 export default function Dashboard() {
+  const { path, url } = useRouteMatch();
   const { scenarioId } = useParams();
   const history = useHistory();
   const [scenarioGroupInfo, setScenarioGroupInfo] = useState([]);
-  const { currentScenario } = useContext(ScenarioContext);
-  const { scenes } = useContext(SceneContext);
-  const [graphLoading, setGraphLoading] = useState(true);
+  const [scenario, setCurrentSecnario] = useState({});
+  const [scenes, setScenes] = useState([]);
+  useGet(`api/scenario/${scenarioId}`, setCurrentSecnario);
+  useGet(`api/scenario/${scenarioId}/scene/all`, setScenes);
+
   const { isLoading } = useGet(
     `/api/group/scenario/${scenarioId}`,
     setScenarioGroupInfo
   );
-  console.log(scenarioGroupInfo)
+  // Check what page we are on
+  const matchViewGroup = useRouteMatch(`${path}/view-group/:groupId`);
+  const backURL = matchViewGroup ? url : "/";
+  const isViewGroupMode = Boolean(matchViewGroup);
+  const viewGroupId = matchViewGroup?.params.groupId || 0;
+  const [groupInfo, setGroupInfo] = useState({});
+  const [heading, setHeading] = useState("");
+  useEffect(() => {
+    setHeading(scenario.name);
+  }, [scenario]);
+  // Fetch group data if in view group mode, skips if not
+  useGet(
+    `/api/group/retrieve/${viewGroupId}`,
+    setGroupInfo,
+    true,
+    !isViewGroupMode
+  );
+
   const viewGroup = async (groupId) => {
-    history.push(`/dashboard/${scenarioId}/view-group/${groupId}`);
+    history.push(`${url}/view-group/${groupId}`);
   };
 
+  // Resets the current groupInfo when the mode is not the view group mode
+  useEffect(() => {
+    if (isViewGroupMode) return;
+    setGroupInfo({});
+  }, [matchViewGroup]);
+
+  // Changes the heading depending on state of page
+  useEffect(() => {
+    if (!isViewGroupMode) {
+      setHeading(scenario.name);
+      return;
+    }
+    if (Object.keys(groupInfo).length == 0) return;
+    setHeading(`Group: ${groupInfo.users[0].name}`);
+  }, [groupInfo]);
+
+  // Create the graph data, also passes if the current group info if it's present
   const { nodes, edges, groupEdges, groupPath, sceneMap } = useMemo(() => {
-    return CreateGraphData(scenes, []);
-  }, [scenes]);
+    return CreateGraphData(scenes, groupInfo ? groupInfo : []);
+  }, [scenes, groupInfo]);
 
   const DashGroupInfo = ({ groupData, className }) => {
     const totalGroups = groupData.length;
@@ -50,7 +84,6 @@ export default function Dashboard() {
     const groupsStarted = groupData.filter(
       (group) => group.path.length != 0
     ).length;
-    // Placeholder here, needs an update for when the group completes a scenario it sets a var in the database, probs alr exist but will implement this later
     return (
       <div
         className={`${className} inline-grid lg:stats-vertical xl:stats-horizontal shadow-(--color-base-content-box-shadow) w-full`}
@@ -82,57 +115,54 @@ export default function Dashboard() {
       </div>
     );
   };
-  /**
-   * Is it better to just show a loading screen instead of conditionally rendering the content to stop it
-   * flashing?
-   *
-   * Todo: Add a button to the edit page and manage groups thingy :D
-   */
+
   return (
-      <ScreenContainer vertical>
-        <DashTopBar>
-          {/* Need to change the help button to maybe be able to pass something down like have a file of help messages which can be accessed and passed down to be page specific */}
-          <HelpButton />
-        </DashTopBar>
-          <div className="h-full px-10 py-7 overflow-y-scroll ">
-            <h1 className="text-3xl font-mona font-bold my-3 flex items-center gap-3">
-              {scenarioGroupInfo.length == 0
-                ? `No groups assgined for ${currentScenario.name}`
-                : `Viewing group${scenarioGroupInfo.length == 1 ? "" : "s"} for ${currentScenario.name}`}
-            </h1>
-            <div className="flex gap-10">
-              <div className="w-[50%]">
+    <ScreenContainer vertical>
+      <DashTopBar back={backURL}>
+        {/* Need to change the help button to maybe be able to pass something down like have a file of help messages which can be accessed and passed down to be page specific */}
+        <HelpButton />
+      </DashTopBar>
+      <div className="h-full px-10 py-7 overflow-y-scroll ">
+        <h1 className="text-3xl font-mona font-bold my-3 flex items-center gap-3">
+          {heading ? heading : <span className="invisible">placeholder</span>}
+        </h1>
+        <div className="flex gap-10">
+          {/* Left side coloumn */}
+          <div className="w-1/2 min-w-0">
+            <Switch>
+              <ProtectedRoute exact path={path}>
                 <DashGroupInfo
                   className={"lg:stats-horizontal mb-10"}
                   groupData={scenarioGroupInfo}
                 />
                 {!isLoading && (
                   <DashGroupTable
-                   groupInfo={scenarioGroupInfo}
-                   rowClick={viewGroup}
-                 />
-
-                )}
-              </div>
-              <div className="w-[50%]">
-                <h1 className="text-3xl font-mona font-bold">Scenario Overview</h1>
-                <ReactFlowProvider>
-                  <ScenarioGraph
-                      inNodes={nodes}
-                      inEdges={edges}
-                      inGPathEdges={groupEdges}
-                      inGPath={groupPath}
-                      inSceneMap={sceneMap}
-                      onLoaded={() => {
-                      setGraphLoading(false);
-                      }}
-                      className="h-[700px]"
+                    groupInfo={scenarioGroupInfo}
+                    rowClick={viewGroup}
                   />
-                </ReactFlowProvider>
-              </div>
-            </div>
+                )}
+              </ProtectedRoute>
+              <ProtectedRoute path={`${path}/view-group/:groupId`}>
+                <ViewGroup />
+              </ProtectedRoute>
+            </Switch>
           </div>
-
-      </ScreenContainer>
+          {/* Right side coloum - used for graph */}
+          <div className="w-1/2 min-w-0">
+            <h3 className="text-3xl font-mona font-bold">Scenario Overview</h3>
+            <ReactFlowProvider>
+              <ScenarioGraph
+                inNodes={nodes}
+                inEdges={edges}
+                inGPathEdges={groupEdges}
+                inGPath={groupPath}
+                inSceneMap={sceneMap}
+                className="h-[700px]"
+              />
+            </ReactFlowProvider>
+          </div>
+        </div>
+      </div>
+    </ScreenContainer>
   );
 }
