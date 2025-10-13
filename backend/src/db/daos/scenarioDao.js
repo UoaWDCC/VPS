b5cc500f5d3276770ea04d2288761fc6cdaca3ed
hazikchaudhry <hazikali3@gmail.com>
2025-10-13 16:19:14 +1300

    made some requested backend changes

--- backend/src/db/daos/scenarioDao.js
+++ backend/src/db/daos/scenarioDao.js
@@ -101,27 +101,17 @@ const retrieveRoleList = async (scenarioId) => {
 const updateScenario = async (scenarioId, updatedScenario) => {
   const scenario = await Scenario.findById(scenarioId);
 
-  // Handle name update
-  if (updatedScenario.name !== undefined) {
-    const previousName = scenario.name;
+ 
+  if (updatedScenario.name?.trim()) {
     scenario.name = updatedScenario.name;
-
-    // if new name is empty, set name to old name
-    if (
-      scenario.name === "" ||
-      scenario.name == null ||
-      scenario.name.trim() === ""
-    ) {
-      scenario.name = previousName;
-    }
   }
 
-  // Handle description update
+ 
   if (updatedScenario.description !== undefined) {
     scenario.description = updatedScenario.description;
   }
 
-  // Handle estimatedTime update
+  
   if (updatedScenario.estimatedTime !== undefined) {
     scenario.estimatedTime = updatedScenario.estimatedTime;
   }
