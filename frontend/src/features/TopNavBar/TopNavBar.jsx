5ee24bf4a6897a15003f7f03713078b443b7d37e
Jauhar <jauhargoga@gmail.com>
2026-04-18 19:20:04 +1200

    style: added logout button on playpage next to back

--- frontend/src/features/TopNavBar/TopNavBar.jsx
+++ frontend/src/features/TopNavBar/TopNavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import { ArrowLeftIcon } from "lucide-react";
 
 export default function TopNavBar({
   onLogout,
@@ -9,11 +10,29 @@ export default function TopNavBar({
 }) {
   const history = useHistory();
 
+  function goBack() {
+    history.push("/");
+  }
+
   return (
     <div className="top-nav-bar">
       <div className="nav-left">
-        <button className="back-btn" onClick={() => history.push("/")}>
+        <button className="back-btn" onClick={goBack}>
+          <ArrowLeftIcon size={20} />
           Back
+        </button>
+        <button className="logout-btn" onClick={onLogout}>
+          <svg
+            className="logout-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.59L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z"
+            />
+          </svg>
+          <span>Logout</span>
         </button>
       </div>
 
@@ -32,28 +51,14 @@ export default function TopNavBar({
         </button>
         <button
           className={`nav-btn ${activeTab === "create" ? "nav-btn-active" : ""}`}
-          onClick={activeTab === "create" ? undefined : onCreate} // only set onClick if not active
+          onClick={activeTab === "create" ? undefined : onCreate} // only sets onClick if not active
         >
           Create & Edit
         </button>
       </div>
 
-      <div className="nav-right">
-        {/* button is currently sitting in center needs to be right side, ill fix later  */}
-        {/* <button className="logout-btn" onClick={onLogout}>
-          <svg
-            className="logout-icon"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fill="currentColor"
-              d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.59L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z"
-            />
-          </svg>
-          <span>Logout</span>
-        </button> */}
-      </div>
+      {/* Logout button should be in nav-right */}
+      <div className="nav-right"></div>
     </div>
   );
 }
