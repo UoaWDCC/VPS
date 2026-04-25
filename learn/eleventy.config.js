5d026f5ed5f3b7605ddac0b23b2d465388e01a19
harbassan <harbassan@hotmail.com>
2026-04-25 23:05:49 +1200

    chore: add basic styles to user docs

--- learn/eleventy.config.js
+++ learn/eleventy.config.js
@@ -1,9 +1,10 @@
 import markdownIt from "markdown-it";
 import markdownItAnchor from "markdown-it-anchor";
 
-export default function (eleventyConfig) {
+export default function(eleventyConfig) {
   const md = markdownIt().use(markdownItAnchor, {
     permalink: markdownItAnchor.permalink.headerLink(), // makes the heading itself a link
   });
   eleventyConfig.setLibrary("md", md);
+  eleventyConfig.addPassthroughCopy("styles");
 }
