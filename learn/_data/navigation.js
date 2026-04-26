428a7c54ea3094da133ae9af7b441f88e279b568
harbassan <harbassan@hotmail.com>
2026-04-26 18:09:04 +1200

    chore: remove unused files and create style overwrite

--- learn/_data/navigation.js
+++
@@ -1,7 +0,0 @@
-export default [
-  { title: "Getting Started", url: "/" },
-  { title: "Editor", url: "/editor/" },
-  { title: "Assignment", url: "/assignment/" },
-  { title: "Dashboard", url: "/dashboard/" },
-  { title: "Multiplayer", url: "/multiplayer/" },
-];
