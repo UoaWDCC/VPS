c4d55bc416996b0b5184148f771b7730b7009553
Jauhar Goga <jauhargoga@gmail.com>
2026-05-26 23:08:28 +1200

    quick

--- frontend/src/features/authoring/wrapper/FontInput.tsx
+++ frontend/src/features/authoring/wrapper/FontInput.tsx
@@ -1,3 +1,28 @@
+import { useState } from "react";
+
+function FontFinder(currentSearch: string = "") {
+  const fonts = [
+    "Arial",
+    "Verdana",
+    "Tahoma",
+    "Trebuchet MS",
+    "Times New Roman",
+    "Georgia",
+    "Garamond",
+    "Courier New",
+    "Helvetica",
+  ];
+
+  const filteredFonts =
+    currentSearch.length == 0
+      ? fonts
+      : fonts.filter((font) =>
+          font.toLowerCase().includes(currentSearch.toLowerCase())
+        );
+
+  return filteredFonts;
+}
+
 function FontInput({
   value,
   onChange,
@@ -5,27 +30,36 @@ function FontInput({
   value: string;
   onChange: (value: string) => void;
 }) {
+  const [open, setOpen] = useState(false);
+  const [fonts, setFonts] = useState(FontFinder());
+
   return (
     <>
       <input
         type="text"
-        className="input input-sm h-[28px] w-30"
+        className="input input-sm h-[28px] w-30 relative"
         placeholder="Font Name"
         value={value}
-        onChange={(e) => onChange(e.target.value)}
-        list="fonts"
+        onChange={(e) => {
+          setFonts(FontFinder(e.target.value));
+          onChange(e.target.value);
+        }}
+        onFocus={() => setOpen(true)}
+        onBlur={() => setOpen(false)}
       />
-      <datalist id="fonts">
-        <option value="Arial" />
-        <option value="Verdana" />
-        <option value="Tahoma" />
-        <option value="Trebuchet MS" />
-        <option value="Times New Roman" />
-        <option value="Georgia" />
-        <option value="Garamond" />
-        <option value="Courier New" />
-        <option value="Helvetica" />
-      </datalist>
+      {open && (
+        <div className={"fixed top-0 left-0 w-full h-full"}>
+          <select
+            className={"absolute top-0 left-0 w-full h-full"}
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+          >
+            {fonts.map((font, index) => (
+              <option key={index}>{font}</option>
+            ))}
+          </select>
+        </div>
+      )}
     </>
   );
 }
