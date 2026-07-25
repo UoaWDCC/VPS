3411faaf300d96af234832bdd686c811a6a5c773
rgin216 <100395589+rgin216@users.noreply.github.com>
2026-07-25 20:34:44 +1200

    fix: theme direct-link scene selector

--- frontend/src/features/authoring/components/Select.tsx
+++ frontend/src/features/authoring/components/Select.tsx
@@ -4,6 +4,7 @@ interface SelectInputProps<T> {
   display?: (v: T) => string;
   onChange: (v: T | null) => void;
   nullable?: boolean;
+  disabled?: boolean;
 }
 
 function SelectInput<T>({
@@ -11,44 +12,53 @@ function SelectInput<T>({
   value,
   display,
   nullable = false,
+  disabled = false,
   onChange,
 }: SelectInputProps<T>) {
   const render = display ?? ((v: T) => String(v));
 
   function handleClick(v: T) {
+    if (disabled) return;
     (document.activeElement as HTMLDivElement).blur();
     onChange(v);
   }
 
   return (
-    <div className="dropdown flex-1">
+    <div
+      className={`dropdown flex-1 ${
+        disabled ? "pointer-events-none opacity-50" : ""
+      }`}
+    >
       <div
-        tabIndex={0}
+        tabIndex={disabled ? -1 : 0}
         role="button"
+        aria-disabled={disabled}
         className="justify-start input mb-1 font-normal join-item w-full"
       >
         {value != null ? render(value) : "None"}
       </div>
-      <ul
-        tabIndex={0}
-        className="dropdown-content menu bg-base-300 rounded-box z-1 w-70 p-2 shadow-sm"
-      >
-        {values.map((v, i) => (
-          <li key={i}>
-            <a
-              onClick={() => handleClick(v)}
-              className="block max-w-65 break-words overflow-hidden"
-            >
-              {render(v)}
-            </a>
-          </li>
-        ))}
-        {nullable ? (
-          <li>
-            <a onClick={() => onChange(null)}>None</a>
-          </li>
-        ) : null}
-      </ul>
+      {!disabled && (
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu bg-base-300 rounded-box z-1 w-70 p-2 shadow-sm"
+        >
+          {values.map((v, i) => (
+            <li key={i}>
+              <a
+                onClick={() => handleClick(v)}
+                className="block max-w-65 break-words overflow-hidden"
+              >
+                {render(v)}
+              </a>
+            </li>
+          ))}
+          {nullable ? (
+            <li>
+              <a onClick={() => onChange(null)}>None</a>
+            </li>
+          ) : null}
+        </ul>
+      )}
     </div>
   );
 }
