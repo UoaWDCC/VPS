6af568dd2450aff283b08c86f1290285b35f4665
Jauhar Goga <jauhargoga@gmail.com>
2026-07-27 17:01:31 +1200

    chore: prettier

--- frontend/src/features/authoring/CanvasSideBar/ObjectPropertyEditor.jsx
+++ frontend/src/features/authoring/CanvasSideBar/ObjectPropertyEditor.jsx
@@ -154,82 +154,82 @@ export function ObjectPropertyEditor({ component }) {
 
   return (
     <>
-    <div className="collapse overflow-visible collapse-arrow bg-base-300 rounded-sm text-s">
-      <input type="checkbox" />
-      <div className="collapse-title min-w-0">Object Properties</div>
-      <div className="collapse-content text--1 bg-base-200">
-        <fieldset className="fieldset pt-2">
-          {/* Width and Height num inputs*/}
-          <span className="flex gap-2 justify-between">
-            <label className="label flex-1">Object Width</label>
-            <label className="label flex-1">Object Height</label>
-          </span>
-          <div className="flex gap-2 justify-between">
-            <input
-              className="input flex-1 min-w-0"
-              value={inputWidth}
-              onChange={(e) => {
-                saveProp(e.target.value, "width", setInputWidth);
-              }}
-            />
-            <input
-              className="input flex-1 min-w-0"
-              value={inputHeight}
-              onChange={(e) => {
-                saveProp(e.target.value, "height", setInputHeight);
-              }}
-            />
-          </div>
-
-          {/* positoin x and y num inputs*/}
-          <span className=" flex gap-2 justify-between">
-            <label className="label flex-1">Position X</label>
-            <label className="label flex-1">Position Y</label>
-          </span>
-          <div className="flex justify-between gap-2 w-full">
-            <input
-              className="input flex-1 min-w-0"
-              value={inputX}
-              onChange={(e) => {
-                saveProp(e.target.value, "x", setInputX);
-              }}
-            />
-            <input
-              className="input flex-1 min-w-0"
-              value={inputY}
-              onChange={(e) => {
-                saveProp(e.target.value, "y", setInputY);
-              }}
-            />
-          </div>
-          <label className="label">Angle</label>
-          <div className="flex justify-between">
-            <input
-              className="input flex-1 min-w-0"
-              value={inputAngle}
-              onChange={(e) =>
-                saveProp(e.target.value, "rotation", setInputAngle)
-              }
-            />
-            <div className="ml-6 flex-1">
-              <button
-                type="button"
-                className="hover:bg-stone-800 cursor-pointer rounded-sm"
-                onClick={() => flipComponent("x")}
-              >
-                <SquareCenterlineDashedHorizontal className="m-2" />
-              </button>
-              <button
-                type="button"
-                className="hover:bg-stone-800 cursor-pointer rounded-sm"
-                onClick={() => flipComponent("y")}
-              >
-                <SquareCenterlineDashedVertical className="m-2" />
-              </button>
+      <div className="collapse overflow-visible collapse-arrow bg-base-300 rounded-sm text-s">
+        <input type="checkbox" />
+        <div className="collapse-title min-w-0">Object Properties</div>
+        <div className="collapse-content text--1 bg-base-200">
+          <fieldset className="fieldset pt-2">
+            {/* Width and Height num inputs*/}
+            <span className="flex gap-2 justify-between">
+              <label className="label flex-1">Object Width</label>
+              <label className="label flex-1">Object Height</label>
+            </span>
+            <div className="flex gap-2 justify-between">
+              <input
+                className="input flex-1 min-w-0"
+                value={inputWidth}
+                onChange={(e) => {
+                  saveProp(e.target.value, "width", setInputWidth);
+                }}
+              />
+              <input
+                className="input flex-1 min-w-0"
+                value={inputHeight}
+                onChange={(e) => {
+                  saveProp(e.target.value, "height", setInputHeight);
+                }}
+              />
             </div>
-          </div>
-        </fieldset>
-      </div>
+
+            {/* positoin x and y num inputs*/}
+            <span className=" flex gap-2 justify-between">
+              <label className="label flex-1">Position X</label>
+              <label className="label flex-1">Position Y</label>
+            </span>
+            <div className="flex justify-between gap-2 w-full">
+              <input
+                className="input flex-1 min-w-0"
+                value={inputX}
+                onChange={(e) => {
+                  saveProp(e.target.value, "x", setInputX);
+                }}
+              />
+              <input
+                className="input flex-1 min-w-0"
+                value={inputY}
+                onChange={(e) => {
+                  saveProp(e.target.value, "y", setInputY);
+                }}
+              />
+            </div>
+            <label className="label">Angle</label>
+            <div className="flex justify-between">
+              <input
+                className="input flex-1 min-w-0"
+                value={inputAngle}
+                onChange={(e) =>
+                  saveProp(e.target.value, "rotation", setInputAngle)
+                }
+              />
+              <div className="ml-6 flex-1">
+                <button
+                  type="button"
+                  className="hover:bg-stone-800 cursor-pointer rounded-sm"
+                  onClick={() => flipComponent("x")}
+                >
+                  <SquareCenterlineDashedHorizontal className="m-2" />
+                </button>
+                <button
+                  type="button"
+                  className="hover:bg-stone-800 cursor-pointer rounded-sm"
+                  onClick={() => flipComponent("y")}
+                >
+                  <SquareCenterlineDashedVertical className="m-2" />
+                </button>
+              </div>
+            </div>
+          </fieldset>
+        </div>
       </div>
     </>
   );
