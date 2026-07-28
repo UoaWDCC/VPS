5b93966ae55f44f019766aa8ec7c518770852769
Jauhar Goga <jauhargoga@gmail.com>
2026-07-28 23:37:58 +1200

    fix: safeguard for rotationg

--- frontend/src/features/authoring/CanvasSideBar/ObjectPropertyEditor.jsx
+++ frontend/src/features/authoring/CanvasSideBar/ObjectPropertyEditor.jsx
@@ -58,7 +58,7 @@ export function ObjectPropertyEditor({ component }) {
         y: axis === "y" ? 2 * center.y - v.y : v.y,
       }));
     });
-    modifyComponentProp(component.id, "bounds.rotation", (prev) => 360 - prev);
+    modifyComponentProp(component.id, "bounds.rotation", (prev) => 360 - (prev ?? 0));
   }
 
   function inputValidation(type, v, set, prevValue) {
