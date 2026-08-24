430ee21c0a44bd6bd156d5339f3301d210ff6f59
rgin216 <100395589+rgin216@users.noreply.github.com>
2026-08-24 13:56:49 +1200

    Rename image authoring files (#495)

--- frontend/src/features/authoring/topbar/Topbar.tsx
+++ frontend/src/features/authoring/topbar/Topbar.tsx
@@ -13,7 +13,7 @@ import { undo, redo } from "../scene/history";
 import { bringToFront, sendToBack } from "../scene/operations/component";
 import { useState } from "react";
 import PropertyMenu from "../../../components/Properties/PropertyMenu";
-import ImageCreateMenu from "../images";
+import ImageCreateMenu from "../ImageCreateMenu";
 import ShapeCreateMenu from "./ShapeCreateMenu";
 import BackgroundMenu from "./BackgroundMenu";
 
